f27732c24b26b87b3cb84dd9381895d7f325e023
Barba2k2 <lorenzo.dz@hotmail.com>
2024-08-05 15:08:11 -0300

    refactor: Separate webview logic into a separate controller

--- lib/app/modules/core/auth/home/home_controller.dart
+++ lib/app/modules/core/auth/home/home_controller.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/material.dart';
 import 'package:mobx/mobx.dart';
 
 import '../../../../core/logger/app_logger.dart';
@@ -8,30 +7,24 @@ part 'home_controller.g.dart';
 class HomeController = HomeControllerBase with _$HomeController;
 
 abstract class HomeControllerBase with Store {
-  final DateTime _selectedDate;
-  final List<TextEditingController> _controllers;
   final HomeService _homeService;
 
   HomeControllerBase({
-    required DateTime selectedDate,
-    required List<TextEditingController> controllers,
     required HomeService homeService,
     required AppLogger logger,
-  })  : _selectedDate = selectedDate,
-        _controllers = controllers,
-        _homeService = homeService;
+  }) : _homeService = homeService;
 
   Future<void> saveSchedules() async {
-    final streamerUrls = _controllers
-        .map(
-          (controller) => controller.text,
-        )
-        .toList();
-    await _homeService.saveSchedules(_selectedDate, streamerUrls);
+    // final streamerUrls = _controllers
+    //     .map(
+    //       (controller) => controller.text,
+    //     )
+    //     .toList();
+    // await _homeService.saveSchedules(_selectedDate, streamerUrls);
   }
 
   Future<void> loadSchedules() async {
-    await _homeService.loadSchedules(_selectedDate, _controllers);
+    // await _homeService.loadSchedules(_selectedDate, _controllers);
   }
 
   Future<void> forceUpdateLive() async {
