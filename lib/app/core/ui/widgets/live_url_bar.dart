aa6e2e826ab1c918b5ee539985d8861b8ad782cd
Barba2k2 <lorenzo.dz@hotmail.com>
2024-08-05 11:33:12 -0300

    feat: Add LiveUrlBar widget for displaying the current channel URL

---
+++ lib/app/core/ui/widgets/live_url_bar.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class LiveUrlBar extends StatelessWidget {
+  final String? currentChannel;
+
+  const LiveUrlBar({
+    required this.currentChannel,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 30,
+      color: Colors.purple[300],
+      width: double.infinity,
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      child: Align(
+        alignment: Alignment.centerLeft,
+        child: Text(
+          currentChannel ?? 'https://www.twitch.tv/BootTeam_',
+          style: GoogleFonts.inter(
+            fontSize: 16.0,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+}
