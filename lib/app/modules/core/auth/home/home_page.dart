c0eb251b84880e3c896ee385a61958324eabb91c
Barba2k2 <lorenzo.dz@hotmail.com>
2024-08-05 15:08:01 -0300

    refactor: Separate webview logic into a separate controller

--- lib/app/modules/core/auth/home/home_page.dart
+++ lib/app/modules/core/auth/home/home_page.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 import 'package:webview_windows/webview_windows.dart';
+
+import '../../../../core/ui/widgets/live_url_bar.dart';
+import '../../../../core/ui/widgets/syslurk_app_bar.dart';
+import 'home_controller.dart';
 
 // import '../../../../core/ui/widgets/syslurk_app_bar.dart';
 
@@ -15,12 +21,35 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
+    final homeController = Modular.get<HomeController>();
+
     return Scaffold(
-      // appBar: const SyslurkAppBar(),
-      body: Webview(
-        webViewController,
-        permissionRequested: (url, permissionKind, isUserInitiated) {
-          return Future.value(WebviewPermissionDecision.allow);
+      appBar: const SyslurkAppBar(),
+      body: Observer(
+        builder: (_) {
+          return Stack(
+            children: [
+              Column(
+                children: [
+                  const LiveUrlBar(
+                    currentChannel: 'Canal XYZ',
+                  ),
+                  Expanded(
+                    child: ListView.builder(
+                      itemCount: 1,
+                      itemBuilder: (context, index) {
+                        return const ListTile(
+                          title: Text('Nome'),
+                          subtitle: Text('Horario'),
+                          // trailing: Text(homeController.getStatusText(item['status'])),
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          );
         },
       ),
     );
