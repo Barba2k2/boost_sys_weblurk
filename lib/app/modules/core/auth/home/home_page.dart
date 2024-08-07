3164a2e9294f5332428efc19e891ece5d4e0228d
Barba2k2 <lorenzo.dz@hotmail.com>
2024-08-07 17:12:57 -0300

    refactor: Remove unused code and logging statements in UserRepositoryImpl

--- lib/app/modules/core/auth/home/home_page.dart
+++ lib/app/modules/core/auth/home/home_page.dart
@@ -1,14 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
-import 'package:flutter_modular/flutter_modular.dart';
 import 'package:webview_windows/webview_windows.dart';
 
 import '../../../../core/ui/widgets/live_url_bar.dart';
-import '../../../../core/ui/widgets/schedule_table.dart';
 import '../../../../core/ui/widgets/syslurk_app_bar.dart';
-import 'home_controller.dart';
-
-// import '../../../../core/ui/widgets/syslurk_app_bar.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -22,8 +17,8 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    final homeController = Modular.get<HomeController>();
-    final scheduleScrollController = ScrollController();
+    // final homeController = Modular.get<HomeController>();
+    // final scheduleScrollController = ScrollController();
 
     return Scaffold(
       appBar: const SyslurkAppBar(),
@@ -48,27 +43,28 @@ class _HomePageState extends State<HomePage> {
                       },
                     ),
                   ),
-                  Visibility(
-                    visible: false,
-                    child: Positioned(
-                      bottom: 0,
-                      left: 0,
-                      right: 0,
-                      child: Container(
-                        color: Colors.white.withOpacity(0.90),
-                        child: SizedBox(
-                          height: 200,
-                          child: Scrollbar(
-                            controller: scheduleScrollController,
-                            child: SingleChildScrollView(
-                              controller: scheduleScrollController,
-                              // child: const ScheduleTable(),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
+                  // Next Feature
+                  // Visibility(
+                  //   visible: false,
+                  //   child: Positioned(
+                  //     bottom: 0,
+                  //     left: 0,
+                  //     right: 0,
+                  //     child: Container(
+                  //       color: Colors.white.withOpacity(0.90),
+                  //       child: SizedBox(
+                  //         height: 200,
+                  //         child: Scrollbar(
+                  //           controller: scheduleScrollController,
+                  //           child: SingleChildScrollView(
+                  //             controller: scheduleScrollController,
+                  //             // child: const ScheduleTable(),
+                  //           ),
+                  //         ),
+                  //       ),
+                  //     ),
+                  //   ),
+                  // ),
                 ],
               ),
             ],
