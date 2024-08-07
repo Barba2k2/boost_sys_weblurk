ba85a54552d7996c39be48402fd975f548eebb78
Barba2k2 <lorenzo.dz@hotmail.com>
2024-08-07 11:01:43 -0300

    refactor: Remove deprecated streamer-related code

--- lib/app/service/streamer/streamer_service.dart
+++
@@ -1,8 +0,0 @@
-import '../../models/user_model.dart';
-
-abstract interface class StreamerService {
-  Future<List<UserModel>> fetchUsers();
-  Future<void> registerUser(String nickname, String password, String role);
-  Future<void> deleteUser(int id);
-  Future<void> editUser(int id, String nickname, String password, String role);
-}
