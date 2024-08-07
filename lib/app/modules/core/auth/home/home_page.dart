e4b961f44a6c948eb8c4edf56ddd955716bf33b9
Barba2k2 <lorenzo.dz@hotmail.com>
2024-08-07 10:42:05 -0300

    refactor: Add ScheduleTable widget for displaying schedule data in a table format

--- lib/app/modules/core/auth/home/home_page.dart
+++ lib/app/modules/core/auth/home/home_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter_modular/flutter_modular.dart';
 import 'package:webview_windows/webview_windows.dart';
 
 import '../../../../core/ui/widgets/live_url_bar.dart';
+import '../../../../core/ui/widgets/schedule_table.dart';
 import '../../../../core/ui/widgets/syslurk_app_bar.dart';
 import 'home_controller.dart';
 
@@ -22,6 +23,7 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     final homeController = Modular.get<HomeController>();
+    final scheduleScrollController = ScrollController();
 
     return Scaffold(
       appBar: const SyslurkAppBar(),
@@ -46,11 +48,46 @@ class _HomePageState extends State<HomePage> {
                       },
                     ),
                   ),
+                  Visibility(
+                    visible: false,
+                    child: Positioned(
+                      bottom: 0,
+                      left: 0,
+                      right: 0,
+                      child: Container(
+                        color: Colors.white.withOpacity(0.90),
+                        child: SizedBox(
+                          height: 200,
+                          child: Scrollbar(
+                            controller: scheduleScrollController,
+                            child: SingleChildScrollView(
+                              controller: scheduleScrollController,
+                              // child: const ScheduleTable(),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
                 ],
               ),
             ],
           );
         },
+      ),
+      floatingActionButton: Observer(
+        builder: (_) => FloatingActionButton.extended(
+          onPressed: () {
+            // controller.isScheduleVisible.toggle();
+          },
+          label: const Text(
+            // controller.isScheduleVisible.value ? 'Esconder' : 'Mostrar',
+            'Mostrar',
+          ),
+          icon: const Icon(
+            Icons.arrow_upward_rounded,
+          ),
+        ),
       ),
     );
   }
