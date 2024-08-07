9ce17a9d6d9895576315c692a8e14966fbd0d541
Barba2k2 <lorenzo.dz@hotmail.com>
2024-08-07 11:34:16 -0300

    feat: Add SchedulesRepository interface and implementation

---
+++ lib/app/repositories/schedule/schedule_repository.dart
@@ -0,0 +1,3 @@
+abstract interface class SchedulesRepository {
+  Future<void> fetchSchedule();
+}
---
+++ lib/app/repositories/schedule/schedule_repository_impl.dart
@@ -0,0 +1,31 @@
+import '../../core/exceptions/failure.dart';
+import '../../core/logger/app_logger.dart';
+import '../../core/rest_client/rest_client.dart';
+import 'schedule_repository.dart';
+
+class ScheduleRepositoryImpl implements SchedulesRepository {
+  final RestClient _restClient;
+  final AppLogger _logger;
+
+  ScheduleRepositoryImpl({
+    required RestClient restClient,
+    required AppLogger logger,
+  })  : _restClient = restClient,
+        _logger = logger;
+
+  @override
+  Future<void > fetchSchedule() async {
+    try {
+      final response = await _restClient.auth().get('/schedule/');
+
+      _logger.info('Response data ${response.data}');
+
+      if(response.statusCode == 200) {
+        throw Failure(message: 'Erro ao buscar os agendamentos');
+      }
+    } catch (e, s) {
+      _logger.error('Error on fetch schedule', e, s);
+      throw Failure(message: 'Erro ao buscar os agendamentos');
+    }
+  }
+}
