d98a166f596ca360741aeb97e301f0cacfc0cf04
Barba2k2 <lorenzo.dz@hotmail.com>
2024-08-07 11:34:23 -0300

    refactor: Update RegisterStreamerController to use ScheduleService instead of StreamerService

--- lib/app/modules/core/auth/register_streamer/register_streamer_controller.dart
+++ lib/app/modules/core/auth/register_streamer/register_streamer_controller.dart
@@ -2,9 +2,7 @@ import 'package:mobx/mobx.dart';
 
 import '../../../../core/exceptions/failure.dart';
 import '../../../../core/logger/app_logger.dart';
-import '../../../../core/ui/widgets/messages.dart';
-import '../../../../models/user_model.dart';
-import '../../../../service/streamer/streamer_service.dart';
+import '../../../../service/schedule/schedule_service.dart';
 
 part 'register_streamer_controller.g.dart';
 
@@ -12,17 +10,17 @@ class RegisterStreamerController = RegisterStreamerControllerBase
     with _$RegisterStreamerController;
 
 abstract class RegisterStreamerControllerBase with Store {
-  final StreamerService _streamerService;
+  final ScheduleService _scheduleService;
   final AppLogger _logger;
 
   RegisterStreamerControllerBase({
-    required StreamerService streamerService,
+    required ScheduleService scheduleService,
     required AppLogger logger,
-  })  : _streamerService = streamerService,
+  })  : _scheduleService = scheduleService,
         _logger = logger;
 
-  @observable
-  List<UserModel>? users = [];
+  // @observable
+  // List? users = [];
 
   @observable
   String? errorMessage;
@@ -37,7 +35,7 @@ abstract class RegisterStreamerControllerBase with Store {
 
       errorMessage = null;
 
-      users = await _streamerService.fetchUsers();
+      await _scheduleService.fetchSchedule();
     } catch (e, s) {
       _logger.error('Controller - Error fetching users', e, s);
 
@@ -46,95 +44,6 @@ abstract class RegisterStreamerControllerBase with Store {
       } else {
         errorMessage = 'Erro ao buscar os usuários';
       }
-    } finally {
-      isLoading = false;
-    }
-  }
-
-  @action
-  Future<void> registerUser(
-    String nickname,
-    String password,
-    String role,
-  ) async {
-    try {
-      isLoading = true;
-
-      errorMessage = null;
-
-      await _streamerService.registerUser(nickname, password, role);
-
-      Messages.success('Streamer registrado com sucesso!');
-
-      await fetchUsers();
-    } catch (e) {
-      _logger.error('Controller - Error registering user', e);
-      if (e is Failure) {
-        errorMessage = e.message;
-      } else {
-        errorMessage = 'Erro ao registrar o usuário';
-      }
-      Messages.warning(errorMessage!);
-    } finally {
-      isLoading = false;
-    }
-  }
-
-  @action
-  Future<void> deleteUser(int id) async {
-    try {
-      isLoading = true;
-
-      errorMessage = null;
-
-      await _streamerService.deleteUser(id);
-
-      Messages.success('Streamer deletado com sucesso!');
-
-      await fetchUsers();
-    } catch (e, s) {
-      _logger.error('Controller - Error deleting user', e, s);
-      if (e is Failure) {
-        errorMessage = e.message;
-      } else {
-        errorMessage = 'Erro ao deletar o usuário';
-      }
-      Messages.warning(errorMessage!);
-    } finally {
-      isLoading = false;
-    }
-  }
-
-  @action
-  Future<void> editUser(
-    int id,
-    String nickname,
-    String password,
-    String role,
-  ) async {
-    try {
-      isLoading = true;
-
-      errorMessage = null;
-
-      await _streamerService.editUser(
-        id,
-        nickname,
-        password,
-        role,
-      );
-
-      Messages.success('Streamer atualizado com sucesso!');
-
-      await fetchUsers();
-    } catch (e, s) {
-      _logger.error('Controller - Error updating user', e, s);
-      if (e is Failure) {
-        errorMessage = e.message;
-      } else {
-        errorMessage = 'Erro ao atualizar o usuário';
-      }
-      Messages.warning(errorMessage!);
     } finally {
       isLoading = false;
     }
--- lib/app/modules/core/auth/register_streamer/register_streamer_controller.g.dart
+++ lib/app/modules/core/auth/register_streamer/register_streamer_controller.g.dart
@@ -9,22 +9,6 @@ part of 'register_streamer_controller.dart';
 // ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic, no_leading_underscores_for_local_identifiers
 
 mixin _$RegisterStreamerController on RegisterStreamerControllerBase, Store {
-  late final _$usersAtom =
-      Atom(name: 'RegisterStreamerControllerBase.users', context: context);
-
-  @override
-  List<UserModel>? get users {
-    _$usersAtom.reportRead();
-    return super.users;
-  }
-
-  @override
-  set users(List<UserModel>? value) {
-    _$usersAtom.reportWrite(value, super.users, () {
-      super.users = value;
-    });
-  }
-
   late final _$errorMessageAtom = Atom(
       name: 'RegisterStreamerControllerBase.errorMessage', context: context);
 
@@ -66,38 +50,9 @@ mixin _$RegisterStreamerController on RegisterStreamerControllerBase, Store {
     return _$fetchUsersAsyncAction.run(() => super.fetchUsers());
   }
 
-  late final _$registerUserAsyncAction = AsyncAction(
-      'RegisterStreamerControllerBase.registerUser',
-      context: context);
-
-  @override
-  Future<void> registerUser(String nickname, String password, String role) {
-    return _$registerUserAsyncAction
-        .run(() => super.registerUser(nickname, password, role));
-  }
-
-  late final _$deleteUserAsyncAction = AsyncAction(
-      'RegisterStreamerControllerBase.deleteUser',
-      context: context);
-
-  @override
-  Future<void> deleteUser(int id) {
-    return _$deleteUserAsyncAction.run(() => super.deleteUser(id));
-  }
-
-  late final _$editUserAsyncAction =
-      AsyncAction('RegisterStreamerControllerBase.editUser', context: context);
-
-  @override
-  Future<void> editUser(int id, String nickname, String password, String role) {
-    return _$editUserAsyncAction
-        .run(() => super.editUser(id, nickname, password, role));
-  }
-
   @override
   String toString() {
     return '''
-users: ${users},
 errorMessage: ${errorMessage},
 isLoading: ${isLoading}
     ''';
