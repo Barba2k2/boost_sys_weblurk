da3167f05e4e882444d8d010631793d9564c932a
Barba2k2 <lorenzo.dz@hotmail.com>
2024-08-07 10:41:36 -0300

    refactor: Update UIConfig theme with button and floating action button styles

--- lib/app/core/ui/ui_config.dart
+++ lib/app/core/ui/ui_config.dart
@@ -6,13 +6,20 @@ class UiConfig {
   static String title = 'Painel de Agendamento';
 
   static ThemeData get theme => ThemeData(
-    useMaterial3: false,
-    primaryColor: Colors.purple.shade900,
-    primaryColorDark: Colors.purple,
-    primaryColorLight: Colors.purple.shade400,
-    appBarTheme: const AppBarTheme(
-      backgroundColor: Colors.purple,
-      centerTitle: true,
-    ),
-  );
+        useMaterial3: false,
+        primaryColor: Colors.purple.shade900,
+        primaryColorDark: Colors.purple,
+        primaryColorLight: Colors.purple.shade400,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.purple,
+          centerTitle: true,
+        ),
+        buttonTheme: const ButtonThemeData(
+          buttonColor: Colors.purple,
+          textTheme: ButtonTextTheme.primary,
+        ),
+        floatingActionButtonTheme: const FloatingActionButtonThemeData(
+          backgroundColor: Colors.purple,
+        ),
+      );
 }
