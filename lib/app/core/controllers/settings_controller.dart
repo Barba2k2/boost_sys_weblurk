c5935c09199760283ffa4cea2e5f78c78c03a704
Barba2k2 <lorenzo.dz@hotmail.com>
2024-08-09 15:43:42 -0300

    refactor: Add terminateApp and muteAppAudio methods to SettingsController

--- lib/app/core/controllers/settings_controller.dart
+++ lib/app/core/controllers/settings_controller.dart
@@ -1,9 +1,10 @@
+import 'dart:io';
 import 'package:mobx/mobx.dart';
-
 import '../../service/schedule/schedule_service.dart';
 import '../formatters/date_formatter.dart';
 import '../logger/app_logger.dart';
 import '../ui/widgets/messages.dart';
+
 part 'settings_controller.g.dart';
 
 class SettingsController = SettingsControllerBase with _$SettingsController;
@@ -17,6 +18,41 @@ abstract class SettingsControllerBase with Store {
     required ScheduleService scheduleService,
   })  : _logger = logger,
         _scheduleService = scheduleService;
+
+  // Método para encerrar o aplicativo
+  @action
+  Future<void> terminateApp() async {
+    try {
+      if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
+        exit(0);
+      } else {
+        throw 'Plataforma não suportada para encerramento do app';
+      }
+    } catch (e) {
+      _logger.error('Erro ao encerrar o aplicativo: $e');
+      Messages.alert('Erro ao encerrar o aplicativo');
+    }
+  }
+
+  // Método para mutar o áudio do WebView
+  @action
+  Future<void> muteAppAudio() async {
+    try {
+      if (Platform.isWindows) {
+        Messages.info('Funcionalidade ainda não implementada');
+      }
+      // else if (Platform.isMacOS) {
+      //   await shell.run('osascript -e "set volume output muted true"');
+      // } else if (Platform.isLinux) {
+      //   await shell.run('amixer -q -D pulse sset Master mute');
+      // } else {
+      //   throw 'Plataforma não suportada para mutar áudio';
+      // }
+    } catch (e) {
+      _logger.error('Erro ao mutar o áudio do aplicativo: $e');
+      Messages.alert('Erro ao mutar o áudio do aplicativo');
+    }
+  }
 
   Future<void> fetchSchedule() async {
     try {
@@ -63,7 +99,6 @@ abstract class SettingsControllerBase with Store {
       ).toList();
 
       // schedule.assignAll(tempSchedule);
-
       // _startChannelChangeTimer();
     } catch (e, s) {
       _logger.error('Error on fetch schedule', e, s);
