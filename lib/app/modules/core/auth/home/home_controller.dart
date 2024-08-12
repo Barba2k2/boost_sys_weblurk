7ce067a518dc77feb1dd09e04cef059c4fea9e0e
Barba2k2 <lorenzo.dz@hotmail.com>
2024-08-12 10:13:39 -0300

    refactor: Update HomeController's onInit method to handle user authentication and navigation

--- lib/app/modules/core/auth/home/home_controller.dart
+++ lib/app/modules/core/auth/home/home_controller.dart
@@ -1,9 +1,11 @@
+import 'package:flutter_modular/flutter_modular.dart';
 import 'package:mobx/mobx.dart';
 import 'package:webview_windows/webview_windows.dart';
 import '../../../../core/exceptions/failure.dart';
 import '../../../../core/logger/app_logger.dart';
 import '../../../../core/ui/widgets/messages.dart';
 import '../../../../service/home/home_service.dart';
+import '../auth_store.dart';
 
 part 'home_controller.g.dart';
 
@@ -12,12 +14,15 @@ class HomeController = HomeControllerBase with _$HomeController;
 abstract class HomeControllerBase with Store {
   final HomeService _homeService;
   final AppLogger _logger;
+  final AuthStore _authStore;
 
   HomeControllerBase({
     required HomeService homeService,
     required AppLogger logger,
+    required AuthStore authStore,
   })  : _homeService = homeService,
-        _logger = logger;
+        _logger = logger,
+        _authStore = authStore;
 
   @observable
   late Future<void> initializationFuture;
@@ -72,8 +77,14 @@ abstract class HomeControllerBase with Store {
     }
   }
 
+  @action
   void onInit() {
-    initializationFuture = initializeWebView();
-    loadSchedules();
+    if (_authStore.userLogged == null ||
+        _authStore.userLogged!.nickname.isEmpty) {
+      Modular.to.navigate('/auth/login/');
+    } else {
+      initializationFuture = initializeWebView();
+      loadSchedules();
+    }
   }
 }
