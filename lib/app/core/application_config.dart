006234496425892c7e557d88b1fe96bfa72e4649
Barba2k2 <lorenzo.dz@hotmail.com>
2024-08-12 22:12:02 -0300

    refactor: Add window manager configuration to ApplicationConfig

--- lib/app/core/application_config.dart
+++ lib/app/core/application_config.dart
@@ -1,12 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:window_manager/window_manager.dart';
 
 import 'helpers/environments.dart';
 
 class ApplicationConfig {
   Future<void> consfigureApp() async {
-    WidgetsFlutterBinding.ensureInitialized();
     await _loadEnvs();
+
+    await _configureWindowManager();
   }
 
   Future<void> _loadEnvs() => Environments.loadEnvs();
+
+  Future<void> _configureWindowManager() async {
+    WidgetsFlutterBinding.ensureInitialized();
+
+    await windowManager.ensureInitialized();
+
+    windowManager.setSize(
+      const Size(800, 600),
+    );
+
+    windowManager.setResizable(false);
+  }
 }
