70ce5256e7f55fa6d914830487fd5007450bd902
Barba2k2 <lorenzo.dz@hotmail.com>
2024-08-12 22:12:15 -0300

    refactor: Remove unused UI widgets and update dependencies

--- lib/app/core/controllers/settings_controller.dart
+++ lib/app/core/controllers/settings_controller.dart
@@ -10,14 +10,11 @@ part 'settings_controller.g.dart';
 class SettingsController = SettingsControllerBase with _$SettingsController;
 
 abstract class SettingsControllerBase with Store {
-  final ScheduleService _scheduleService;
   final AppLogger _logger;
 
   SettingsControllerBase({
     required AppLogger logger,
-    required ScheduleService scheduleService,
-  })  : _logger = logger,
-        _scheduleService = scheduleService;
+  })  : _logger = logger;
 
   // Método para encerrar o aplicativo
   @action
@@ -51,60 +48,6 @@ abstract class SettingsControllerBase with Store {
     } catch (e) {
       _logger.error('Erro ao mutar o áudio do aplicativo: $e');
       Messages.alert('Erro ao mutar o áudio do aplicativo');
-    }
-  }
-
-  Future<void> fetchSchedule() async {
-    try {
-      final scheduleData = await _scheduleService.fetchSchedule();
-      DateTime now = DateTime.now();
-
-      var tempSchedule = scheduleData.where(
-        (item) {
-          DateTime streamDate = DateTime.parse(item['date']);
-          return streamDate.day == now.day &&
-              streamDate.month == now.month &&
-              streamDate.year == now.year;
-        },
-      ).map(
-        (item) {
-          DateTime streamDate = DateTime.parse(item['date']);
-          DateTime startTime = DateTime(
-            streamDate.year,
-            streamDate.month,
-            streamDate.day,
-            int.parse(item['startTime'].split(':')[0]),
-            int.parse(item['startTime'].split(':')[1]),
-          );
-          int status;
-
-          if (startTime.hour == now.hour) {
-            status = 1; // Live em andamento
-          } else if (startTime.isBefore(now)) {
-            status = 2; // Live já realizada
-          } else {
-            status = 0; // Live não realizada
-          }
-
-          return {
-            'seq': startTime.hour + 1,
-            'nome_do_streamer': item['streamerUrl']?.split('/')?.last ?? '',
-            'horario': DateFormatter.formatTime(startTime),
-            'data': DateFormatter.formatDate(streamDate),
-            'acao': '2 - Executar Live Programada',
-            'link_do_canal': item['streamerUrl'] ?? '',
-            'status': status,
-          };
-        },
-      ).toList();
-
-      // schedule.assignAll(tempSchedule);
-      // _startChannelChangeTimer();
-    } catch (e, s) {
-      _logger.error('Error on fetch schedule', e, s);
-      Messages.alert(
-        'Erro ao atualizar as listas, verifique se tem conexão com a internet e tente novamente.',
-      );
     }
   }
 }
--- lib/app/core/controllers/settings_controller.g.dart
+++ lib/app/core/controllers/settings_controller.g.dart
@@ -9,6 +9,22 @@ part of 'settings_controller.dart';
 // ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic, no_leading_underscores_for_local_identifiers
 
 mixin _$SettingsController on SettingsControllerBase, Store {
+  late final _$terminateAppAsyncAction =
+      AsyncAction('SettingsControllerBase.terminateApp', context: context);
+
+  @override
+  Future<void> terminateApp() {
+    return _$terminateAppAsyncAction.run(() => super.terminateApp());
+  }
+
+  late final _$muteAppAudioAsyncAction =
+      AsyncAction('SettingsControllerBase.muteAppAudio', context: context);
+
+  @override
+  Future<void> muteAppAudio() {
+    return _$muteAppAudioAsyncAction.run(() => super.muteAppAudio());
+  }
+
   @override
   String toString() {
     return '''
