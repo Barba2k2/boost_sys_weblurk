7b718c4b71ae2571869ab5fd2c44cbc9496411e0
Barba2k2 <lorenzo.dz@hotmail.com>
2024-08-12 22:13:23 -0300

    refactor: Update home page to load current channel and initialize web view

--- lib/app/modules/core/auth/home/home_page.dart
+++ lib/app/modules/core/auth/home/home_page.dart
@@ -1,7 +1,8 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_modular/flutter_modular.dart';
-import 'package:webview_windows/webview_windows.dart';
 
 import '../../../../core/ui/widgets/live_url_bar.dart';
 import '../../../../core/ui/widgets/syslurk_app_bar.dart';
@@ -17,75 +18,111 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   final homeController = Modular.get<HomeController>();
-  final webViewController = WebviewController();
 
   @override
   void initState() {
     super.initState();
     homeController.onInit();
+    homeController.loadCurrentChannel();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: SyslurkAppBar(),
-      body: Observer(
-        builder: (_) {
-          return Stack(
-            children: [
-              Column(
+    log('Current channel: ${homeController.currentChannel}');
+
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return Scaffold(
+          appBar: SyslurkAppBar(),
+          body: Observer(
+            builder: (_) {
+              return Stack(
                 children: [
-                  const LiveUrlBar(
-                    currentChannel: 'Canal XYZ',
+                  Column(
+                    children: [
+                      FutureBuilder<void>(
+                        future: homeController.loadCurrentChannel(),
+                        builder: (context, snapshot) {
+                          if (snapshot.connectionState ==
+                              ConnectionState.done) {
+                            return LiveUrlBar(
+                              currentChannel: homeController.currentChannel,
+                            );
+                          } else {
+                            return const Center(
+                              child: LinearProgressIndicator(
+                                backgroundColor: Colors.purple,
+                              ),
+                            );
+                          }
+                        },
+                      ),
+                      FutureBuilder(
+                        future: homeController.webViewController.initialize(),
+                        builder: (context, snapshot) {
+                          if (snapshot.connectionState ==
+                              ConnectionState.done) {
+                            return Expanded(
+                              child: WebviewWidget(
+                                webViewController:
+                                    homeController.webViewController,
+                                initializationFuture:
+                                    homeController.initializationFuture,
+                              ),
+                            );
+                          } else {
+                            return const Center(
+                              child: CircularProgressIndicator.adaptive(
+                                backgroundColor: Colors.purple,
+                              ),
+                            );
+                          }
+                        },
+                      ),
+                      // Next Feature
+                      // Visibility(
+                      //   visible: false,
+                      //   child: Positioned(
+                      //     bottom: 0,
+                      //     left: 0,
+                      //     right: 0,
+                      //     child: Container(
+                      //       color: Colors.white.withOpacity(0.90),
+                      //       child: SizedBox(
+                      //         height: 200,
+                      //         child: Scrollbar(
+                      //           controller: scheduleScrollController,
+                      //           child: SingleChildScrollView(
+                      //             controller: scheduleScrollController,
+                      //             // child: const ScheduleTable(),
+                      //           ),
+                      //         ),
+                      //       ),
+                      //     ),
+                      //   ),
+                      // ),
+                    ],
                   ),
-                  Expanded(
-                    child: WebviewWidget(
-                      webViewController: webViewController,
-                      initializationFuture: homeController.initializationFuture,
-                    ),
-                  ),
-                  // Next Feature
-                  // Visibility(
-                  //   visible: false,
-                  //   child: Positioned(
-                  //     bottom: 0,
-                  //     left: 0,
-                  //     right: 0,
-                  //     child: Container(
-                  //       color: Colors.white.withOpacity(0.90),
-                  //       child: SizedBox(
-                  //         height: 200,
-                  //         child: Scrollbar(
-                  //           controller: scheduleScrollController,
-                  //           child: SingleChildScrollView(
-                  //             controller: scheduleScrollController,
-                  //             // child: const ScheduleTable(),
-                  //           ),
-                  //         ),
-                  //       ),
-                  //     ),
-                  //   ),
-                  // ),
                 ],
-              ),
-            ],
-          );
-        },
-      ),
-      floatingActionButton: Observer(
-        builder: (_) => FloatingActionButton.extended(
-          onPressed: () {
-            // controller.isScheduleVisible.toggle();
-          },
-          label: const Text(
-            // controller.isScheduleVisible.value ? 'Esconder' : 'Mostrar',
-            'Mostrar',
+              );
+            },
           ),
-          icon: const Icon(
-            Icons.arrow_upward_rounded,
-          ),
-        ),
-      ),
+          // floatingActionButton: Observer(
+          //   builder: (_) => FloatingActionButton.extended(
+          //     onPressed: () {
+          //       // controller.isScheduleVisible.toggle();
+          //     },
+          //     label: const Text(
+          //       // controller.isScheduleVisible.value ? 'Esconder' : 'Mostrar',
+          //       'Mostrar',
+          //     ),
+          //     icon: const Icon(
+          //       Icons.arrow_upward_rounded,
+          //     ),
+          //   ),
+          // ),
+        );
+      },
     );
   }
 }
