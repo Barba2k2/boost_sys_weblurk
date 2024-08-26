a47fda4e3d1135de929a8f409b5e3b8b8b7244e1
Barba2k2 <lorenzo.dz@hotmail.com>
2024-08-26 16:05:59 -0300

    refactor: Update AuthStoreBase to include logging for missing Streamer ID in loaded user data

--- lib/app/modules/core/auth/auth_store.dart
+++ lib/app/modules/core/auth/auth_store.dart
@@ -5,6 +5,7 @@ import 'package:mobx/mobx.dart';
 
 import '../../../core/helpers/constants.dart';
 import '../../../core/local_storage/local_storage.dart';
+import '../../../core/logger/app_logger.dart';
 import '../../../models/user_model.dart';
 
 part 'auth_store.g.dart';
@@ -13,6 +14,7 @@ class AuthStore = AuthStoreBase with _$AuthStore;
 
 abstract class AuthStoreBase with Store {
   final LocalStorage _localStorage;
+  final _logger = Modular.get<AppLogger>();
 
   @readonly
   UserModel? _userLogged;
@@ -29,6 +31,9 @@ abstract class AuthStoreBase with Store {
 
     if (userModelJson != null) {
       _userLogged = UserModel.fromJson(json.decode(userModelJson));
+      if (_userLogged?.id == null || _userLogged!.id == 0) {
+        _logger.warning('Streamer ID is missing in the loaded user data.');
+      }
     } else {
       _userLogged = UserModel.empty();
       await logout();
@@ -39,9 +44,10 @@ abstract class AuthStoreBase with Store {
   Future<void> updateUserStatus(String status) async {
     if (_userLogged != null) {
       _userLogged = _userLogged!.copyWith(status: status);
+      _logger.info('User logged: ${_userLogged!.toJson()}');
       await _localStorage.write(
-        Constants.LOCAL_SOTRAGE_USER_LOGGED_STATUS_KEY,
-        _userLogged!.toJson(),
+        Constants.LOCAL_SOTRAGE_USER_LOGGED_DATA_KEY,
+        json.encode(_userLogged!.toJson()),
       );
     }
   }
