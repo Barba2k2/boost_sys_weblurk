1de2435b39987321787f57ec10e191d2895b32d7
Lorenzo DZ <lorenzo.dz@hotmail.com>
2024-12-09 20:55:50 -0300

    Refactor WebviewWidget to use webview_flutter package, updating the controller setup and removing InAppWebView dependencies. Adjusted onWebViewCreated callback to support the new WebViewController implementation.

--- lib/app/core/ui/widgets/webview_widget.dart
+++ lib/app/core/ui/widgets/webview_widget.dart
@@ -1,62 +1,34 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
 import '../../logger/app_logger.dart';
 
 class WebviewWidget extends StatelessWidget {
   final String initialUrl;
-  final void Function(InAppWebViewController) onWebViewCreated;
+  final Future<void> Function(WebViewController)? onWebViewCreated;
   final AppLogger? logger;
 
   const WebviewWidget({
     required this.initialUrl,
-    required this.onWebViewCreated,
+    this.onWebViewCreated,
     this.logger,
     super.key,
   });
 
   @override
   Widget build(BuildContext context) {
-    return InAppWebView(
-      initialUrlRequest: URLRequest(
-        url: WebUri(initialUrl),
-      ),
-      initialSettings: InAppWebViewSettings(
-        javaScriptEnabled: true,
-        userAgent:
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 Edge/18.19582",
-        preferredContentMode: UserPreferredContentMode.DESKTOP,
-        mixedContentMode: MixedContentMode.MIXED_CONTENT_ALWAYS_ALLOW,
-        cacheMode: CacheMode.LOAD_DEFAULT,
-        cacheEnabled: true,
-        javaScriptCanOpenWindowsAutomatically: true,
-        supportMultipleWindows: true,
-      ),
-      onWebViewCreated: (controller) {
-        onWebViewCreated(controller);
-      },
-      onLoadStart: (controller, url) {
-        logger?.info('Carregando: $url');
-      },
-      onLoadStop: (controller, url) {
-        logger?.info('Navegação completada: $url');
-      },
-      onLoadError: (controller, url, code, message) {
-        logger?.error('Erro ao carregar $url: $message');
-        logger?.error('Código do erro: $code');
-      },
-      onPermissionRequest: (controller, request) async {
-        logger?.info('Solicitação de permissão de: ${request.origin}');
-        return PermissionResponse(
-          resources: request.resources,
-          action: PermissionResponseAction.GRANT,
-        );
-      },
-      onConsoleMessage: (controller, consoleMessage) {
-        logger?.info('Console message: ${consoleMessage.message}');
-        if (consoleMessage.messageLevel == ConsoleMessageLevel.ERROR) {
-          logger?.error('Erro do console detectado: ${consoleMessage.message}');
-        }
-      },
-    );
+    final WebViewController controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..loadRequest(
+        Uri.parse(initialUrl),
+      );
+
+    // Chama o callback assim que o WebViewController for configurado
+    if (onWebViewCreated != null) {
+      onWebViewCreated!(controller);
+      logger?.info('WebviewWidget: onWebViewCreated callback called');
+    }
+
+    return WebViewWidget(controller: controller);
   }
 }
