f7594bff69440f372a7d6eff421242056a9502c4
Lorenzo DZ <lorenzo.dz@hotmail.com>
2024-12-09 23:13:05 -0300

    Refactor WebviewWidget to convert from Stateful to Stateless, simplifying the widget structure.

--- lib/app/core/ui/widgets/webview_widget.dart
+++ lib/app/core/ui/widgets/webview_widget.dart
@@ -3,7 +3,7 @@ import 'package:webview_flutter/webview_flutter.dart';
 
 import '../../logger/app_logger.dart';
 
-class WebviewWidget extends StatefulWidget {
+class WebviewWidget extends StatelessWidget {
   final String initialUrl;
   final Future<void> Function(WebViewController)? onWebViewCreated;
   final AppLogger? logger;
@@ -16,55 +16,16 @@ class WebviewWidget extends StatefulWidget {
   });
 
   @override
-  State<WebviewWidget> createState() => _WebviewWidgetState();
-}
-
-class _WebviewWidgetState extends State<WebviewWidget> {
-  late final WebViewController _controller;
-  bool _isInitialized = false;
-
-  @override
-  void initState() {
-    super.initState();
-    _initializeController();
-  }
-
-  Future<void> _initializeController() async {
-    _controller = WebViewController()
-      ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      ..setBackgroundColor(Colors.transparent);
-
-    // Atrasa a inicialização do WebView
-    await Future.delayed(const Duration(milliseconds: 100));
-
-    if (!mounted) return;
-
-    try {
-      await _controller.loadRequest(Uri.parse(widget.initialUrl));
-
-      if (!mounted) return;
-
-      setState(() {
-        _isInitialized = true;
-      });
-
-      // Notifica apenas após a inicialização completa
-      if (widget.onWebViewCreated != null) {
-        await widget.onWebViewCreated!(_controller);
-      }
-    } catch (e) {
-      widget.logger?.error('Erro ao inicializar WebView', e);
-    }
-  }
-
-  @override
   Widget build(BuildContext context) {
-    if (!_isInitialized) {
-      return const Center(
-        child: CircularProgressIndicator(),
-      );
+    final controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..loadRequest(Uri.parse(initialUrl));
+
+    if (onWebViewCreated != null) {
+      onWebViewCreated!(controller);
+      logger?.info('WebviewWidget: onWebViewCreated callback called');
     }
 
-    return WebViewWidget(controller: _controller);
+    return WebViewWidget(controller: controller);
   }
 }
