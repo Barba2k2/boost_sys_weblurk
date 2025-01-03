a66b6e40aac08c663b2a7e2e7e3104d0140dc6f7
Lorenzo DZ <lorenzo.dz@hotmail.com>
2025-01-02 22:56:15 -0300

    feat(home_module): Adds WebView and Polling services to HomeModule

--- lib/app/modules/core/auth/home/home_module.dart
+++ lib/app/modules/core/auth/home/home_module.dart
@@ -6,9 +6,12 @@ import '../../../../repositories/home/home_repository.dart';
 import '../../../../repositories/home/home_repository_impl.dart';
 import '../../../../service/home/home_service.dart';
 import '../../../../service/home/home_service_impl.dart';
+import 'services/polling_services.dart';
+import 'services/webview_service.dart';
 import '../auth_store.dart';
 import 'home_controller.dart';
 import 'home_page.dart';
+
 
 class HomeModule extends Module {
   @override
@@ -25,11 +28,24 @@ class HomeModule extends Module {
             logger: i<AppLogger>(),
           ),
         ),
+        Bind.lazySingleton<WebViewService>(
+          (i) => WebViewServiceImpl(
+            logger: i<AppLogger>(),
+          ),
+        ),
+        Bind.lazySingleton<PollingService>(
+          (i) => PollingServiceImpl(
+            homeService: i<HomeService>(),
+            logger: i<AppLogger>(),
+          ),
+        ),
         Bind.lazySingleton<HomeController>(
           (i) => HomeController(
             homeService: i<HomeService>(),
             authStore: i<AuthStore>(),
             logger: i<AppLogger>(),
+            webViewService: i<WebViewService>(),
+            pollingService: i<PollingService>(),
           ),
         ),
       ];
