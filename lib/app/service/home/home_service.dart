8581e39eb3f7bf220a8f19f43e0faaa5e221b56f
Lorenzo DZ <lorenzo.dz@hotmail.com>
2025-06-29 22:50:18 -0300

    feat: update home service interface

--- lib/app/service/home/home_service.dart
+++ lib/app/service/home/home_service.dart
@@ -1,5 +1,10 @@
+import '../../models/schedule_list_model.dart';
+
 abstract class HomeService {
   Future<void> fetchSchedules();
+  Future<List<ScheduleListModel>> fetchScheduleLists();
+  Future<List<String>> getAvailableListNames();
+  Future<ScheduleListModel?> fetchScheduleListByName(String listName);
   // Future<void> forceUpdateLive();
   Future<void> updateLists();
   Future<String?> fetchCurrentChannel();
