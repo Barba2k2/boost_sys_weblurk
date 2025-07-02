7554edcb7d891ace7abefbb09e78a312b3758b09
Lorenzo DZ <lorenzo.dz@hotmail.com>
2025-07-02 01:25:51 -0300

    refactor: update home repository interface

--- lib/app/repositories/home/home_repository.dart
+++ lib/app/repositories/home/home_repository.dart
@@ -1,8 +1,9 @@
 import '../../models/score_model.dart';
 import '../../models/schedule_list_model.dart';
+import '../../models/schedule_model.dart';
 
 abstract class HomeRepository {
-  Future<List<Map<String, dynamic>>> loadSchedules(DateTime date);
+  Future<List<ScheduleModel>> loadSchedules(DateTime date);
   Future<List<ScheduleListModel>> loadScheduleLists(DateTime date);
   Future<List<String>> getAvailableListNames();
   Future<ScheduleListModel?> loadScheduleListByName(
