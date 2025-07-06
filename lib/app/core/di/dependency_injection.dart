2b86cddbd7603528ea0047e797a6599e89552aea
Lorenzo DZ <lorenzo.dz@hotmail.com>
2025-07-06 15:50:45 -0300

    feat: register HomeService in dependency injection

--- lib/app/core/di/dependency_injection.dart
+++ lib/app/core/di/dependency_injection.dart
@@ -1,19 +1,7 @@
 import 'package:get_it/get_it.dart';
 import '../../features/auth/domain/entities/auth_store.dart';
-import '../../repositories/home/home_repository.dart';
-import '../../repositories/home/home_repository_impl.dart';
-import '../../repositories/schedule/schedule_repository.dart';
-import '../../repositories/schedule/schedule_repository_impl.dart';
-import '../../repositories/user/user_repository.dart';
-import '../../repositories/user/user_repository_impl.dart';
-import '../../service/home/home_service.dart';
-import '../../service/home/home_service_impl.dart';
-import '../../service/schedule/schedule_service.dart';
-import '../../service/schedule/schedule_service_impl.dart';
-import '../../service/user/user_service.dart';
-import '../../service/user/user_service_impl.dart';
-import '../../service/webview/windows_web_view_service.dart';
-import '../../service/webview/windows_web_view_service_impl.dart';
+import '../../features/home/data/services/home_service_impl.dart';
+import '../../features/home/domain/services/home_service.dart';
 import '../controllers/settings_controller.dart';
 import '../controllers/url_launch_controller.dart';
 import '../local_storage/flutter_secure_storage/flutter_secure_storage_local_storage_impl.dart';
@@ -51,50 +39,18 @@ class DependencyInjection {
       authStore: get<AuthStore>(),
     ));
 
-    // Repositories
-    _getIt.registerSingleton<UserRepository>(UserRepositoryImpl(
-      logger: get<AppLogger>(),
-      restClient: get<RestClient>(),
-    ));
-
-    _getIt.registerSingleton<HomeRepository>(HomeRepositoryImpl(
-      restClient: get<RestClient>(),
-      logger: get<AppLogger>(),
-    ));
-
-    _getIt.registerSingleton<SchedulesRepository>(ScheduleRepositoryImpl(
-      restClient: get<RestClient>(),
-      logger: get<AppLogger>(),
-    ));
-
-    // Services
-    _getIt.registerSingleton<UserService>(UserServiceImpl(
-      logger: get<AppLogger>(),
-      userRepository: get<UserRepository>(),
-      localStorage: get<LocalStorage>(),
-      localSecureStorage: get<LocalSecureStorage>(),
-    ));
-
-    _getIt.registerSingleton<HomeService>(HomeServiceImpl(
-      homeRepository: get<HomeRepository>(),
-      logger: get<AppLogger>(),
-    ));
-
-    _getIt.registerSingleton<ScheduleService>(StreamerServiceImpl(
-      streamerRepository: get<SchedulesRepository>(),
-      logger: get<AppLogger>(),
-    ));
-
-    _getIt.registerSingleton<WindowsWebViewService>(WindowsWebViewServiceImpl(
-      logger: get<AppLogger>(),
-    ));
-
     // Controllers
     _getIt.registerSingleton<UrlLaunchController>(UrlLaunchController(
       logger: get<AppLogger>(),
     ));
 
     _getIt.registerSingleton<SettingsController>(SettingsController(
+      logger: get<AppLogger>(),
+    ));
+
+    // Home services
+    _getIt.registerSingleton<HomeService>(HomeServiceImpl(
+      restClient: get<RestClient>(),
       logger: get<AppLogger>(),
     ));
 
