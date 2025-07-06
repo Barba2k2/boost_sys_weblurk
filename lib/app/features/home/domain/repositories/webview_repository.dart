9908d1ef5cbdfb49b660a324d30b39ea997dc396
Barba2k2 <lorenzo.dz@hotmail.com>
2025-07-06 16:55:40 -0300

    refactor: update webview repository interface

--- lib/app/features/home/domain/repositories/webview_repository.dart
+++ lib/app/features/home/domain/repositories/webview_repository.dart
@@ -1,14 +1,14 @@
-import 'package:webview_windows/webview_windows.dart';
+import 'package:desktop_webview_window/desktop_webview_window.dart';
 
 import '../../../../utils/utils.dart';
 
 abstract class WebViewRepository {
-  Future<Result<void>> initializeWebView(WebviewController controller);
+  Future<Result<void>> initializeWebView(Webview controller);
   Future<Result<void>> loadUrl(String url);
   Future<Result<void>> reload();
   Future<Result<bool>> isResponding();
   bool get isInitialized;
-  WebviewController? get controller;
+  Webview? get controller;
   Stream<bool> get healthStatus;
   void notifyActivity();
   void dispose();
