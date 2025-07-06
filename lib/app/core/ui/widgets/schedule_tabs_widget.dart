794250d6f15fb3e64110dddbeb337452cf2d0dbe
Lorenzo DZ <lorenzo.dz@hotmail.com>
2025-07-06 10:16:25 -0300

    feat: update schedule tabs widget

--- lib/app/core/ui/widgets/schedule_tabs_widget.dart
+++ lib/app/core/ui/widgets/schedule_tabs_widget.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_mobx/flutter_mobx.dart';
-import 'package:flutter_modular/flutter_modular.dart';
 import 'package:google_fonts/google_fonts.dart';
-
-import '../../../modules/core/auth/home/home_controller.dart';
+import '../../../models/schedule_model.dart';
 import 'schedule_table.dart';
 
 class ScheduleTabsWidget extends StatefulWidget {
-  const ScheduleTabsWidget({super.key});
+  final int currentTabIndex;
+  final void Function(int) onTabChanged;
+  final List<ScheduleModel> listaASchedules;
+  final List<ScheduleModel> listaBSchedules;
+
+  const ScheduleTabsWidget({
+    super.key,
+    required this.currentTabIndex,
+    required this.onTabChanged,
+    required this.listaASchedules,
+    required this.listaBSchedules,
+  });
 
   @override
   State<ScheduleTabsWidget> createState() => _ScheduleTabsWidgetState();
@@ -16,16 +24,28 @@ class ScheduleTabsWidget extends StatefulWidget {
 class _ScheduleTabsWidgetState extends State<ScheduleTabsWidget>
     with TickerProviderStateMixin {
   late TabController _tabController;
-  final homeController = Modular.get<HomeController>();
 
   @override
   void initState() {
     super.initState();
     _tabController = TabController(
-      length: 2, // 2 abas fixas: Lista A e Lista B
+      length: 2,
       vsync: this,
-      initialIndex: homeController.currentTabIndex,
+      initialIndex: widget.currentTabIndex,
     );
+    _tabController.addListener(() {
+      if (_tabController.indexIsChanging) {
+        widget.onTabChanged(_tabController.index);
+      }
+    });
+  }
+
+  @override
+  void didUpdateWidget(covariant ScheduleTabsWidget oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (oldWidget.currentTabIndex != widget.currentTabIndex) {
+      _tabController.index = widget.currentTabIndex;
+    }
   }
 
   @override
@@ -36,74 +56,62 @@ class _ScheduleTabsWidgetState extends State<ScheduleTabsWidget>
 
   @override
   Widget build(BuildContext context) {
-    return Observer(
-      builder: (_) {
-        return Column(
-          children: [
-            // TabBar
-            Container(
-              width: double.infinity,
-              decoration: BoxDecoration(
-                color: Colors.white.withValues(alpha: 0.8),
-                borderRadius: const BorderRadius.only(
-                  topRight: Radius.circular(8),
-                ),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withValues(alpha: 0.1),
-                    blurRadius: 4,
-                    offset: const Offset(0, 2),
-                  ),
-                ],
-              ),
-              child: TabBar(
-                controller: _tabController,
-                onTap: (index) => homeController.switchTab(index),
-                labelColor: const Color(0xFF2C1F4A),
-                unselectedLabelColor: Colors.grey[600],
-                indicatorColor: const Color(0xFF2C1F4A),
-                labelStyle: GoogleFonts.inter(
-                  fontWeight: FontWeight.w600,
-                  fontSize: 14,
-                ),
-                unselectedLabelStyle: GoogleFonts.inter(
-                  fontWeight: FontWeight.w400,
-                  fontSize: 14,
-                ),
-                isScrollable: false,
-                indicatorPadding: const EdgeInsets.symmetric(horizontal: 8.0),
-                tabs: const [
-                  Tab(text: 'Lista A', iconMargin: EdgeInsets.zero),
-                  Tab(text: 'Lista B', iconMargin: EdgeInsets.zero),
-                ],
-                labelPadding: EdgeInsets.symmetric(horizontal: 16.0),
-              ),
+    return Column(
+      children: [
+        Container(
+          width: double.infinity,
+          decoration: BoxDecoration(
+            color: Colors.white.withOpacity(0.8),
+            borderRadius: const BorderRadius.only(
+              topRight: Radius.circular(8),
             ),
-            // TabBarView
-            Expanded(
-              child: TabBarView(
-                controller: _tabController,
-                children: [
-                  // Aba Lista A
-                  Observer(
-                    builder: (_) => ScheduleTable(
-                      schedules: homeController.listaASchedules,
-                      listName: 'Lista A',
-                    ),
-                  ),
-                  // Aba Lista B
-                  Observer(
-                    builder: (_) => ScheduleTable(
-                      schedules: homeController.listaBSchedules,
-                      listName: 'Lista B',
-                    ),
-                  ),
-                ],
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.1),
+                blurRadius: 4,
+                offset: const Offset(0, 2),
               ),
+            ],
+          ),
+          child: TabBar(
+            controller: _tabController,
+            onTap: widget.onTabChanged,
+            labelColor: const Color(0xFF2C1F4A),
+            unselectedLabelColor: Colors.grey[600],
+            indicatorColor: const Color(0xFF2C1F4A),
+            labelStyle: GoogleFonts.inter(
+              fontWeight: FontWeight.w600,
+              fontSize: 14,
             ),
-          ],
-        );
-      },
+            unselectedLabelStyle: GoogleFonts.inter(
+              fontWeight: FontWeight.w400,
+              fontSize: 14,
+            ),
+            isScrollable: false,
+            indicatorPadding: const EdgeInsets.symmetric(horizontal: 8.0),
+            tabs: const [
+              Tab(text: 'Lista A', iconMargin: EdgeInsets.zero),
+              Tab(text: 'Lista B', iconMargin: EdgeInsets.zero),
+            ],
+            labelPadding: const EdgeInsets.symmetric(horizontal: 16.0),
+          ),
+        ),
+        Expanded(
+          child: TabBarView(
+            controller: _tabController,
+            children: [
+              ScheduleTable(
+                schedules: widget.listaASchedules,
+                listName: 'Lista A',
+              ),
+              ScheduleTable(
+                schedules: widget.listaBSchedules,
+                listName: 'Lista B',
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
