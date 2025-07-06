84b0cfe11d47a022a0dd8c8b0b5b577c8b910a9a
Lorenzo DZ <lorenzo.dz@hotmail.com>
2025-07-06 15:51:45 -0300

    refactor: update app.dart imports to match new MVVM structure

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -2,7 +2,4 @@
 export 'app_widget.dart';
 export 'core/core.dart';
 export 'features/features.dart';
-export 'models/models.dart';
-export 'repositories/repositories.dart';
-export 'service/service.dart';
 export 'utils/utils.dart';
