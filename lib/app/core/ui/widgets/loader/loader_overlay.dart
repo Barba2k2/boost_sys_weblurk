e2f8661be847d00c95e13b5621b1a3409a51ee27
Barba2k2 <lorenzo.dz@hotmail.com>
2025-07-06 18:22:43 -0300

    refactor(ui): break down Loader widget into smaller components
    - Create LoaderOverlay and LoaderTypes components
    - Add Loader barrel file for exports
    - Improve code organization and reusability
    - Follow MVVM architecture patterns

---
+++ lib/app/core/ui/widgets/loader/loader_overlay.dart
@@ -0,0 +1,36 @@
+import 'package:asuka/asuka.dart';
+import 'package:flutter/material.dart';
+
+class LoaderOverlay {
+  LoaderOverlay._();
+
+  static OverlayEntry? _entry;
+  static bool _open = false;
+
+  static void show() {
+    _entry ??= OverlayEntry(
+      builder: (context) => Container(
+        color: Colors.black54,
+        child: const Center(
+          child: CircularProgressIndicator(
+            color: Colors.purple,
+          ),
+        ),
+      ),
+    );
+
+    if (!_open) {
+      _open = true;
+      Asuka.addOverlay(_entry!);
+    }
+  }
+
+  static void hide() {
+    if (_open) {
+      _open = false;
+      _entry?.remove();
+    }
+  }
+
+  static bool get isOpen => _open;
+} 
