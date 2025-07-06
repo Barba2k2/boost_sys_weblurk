dec5d3505d7151e69aa07fbbe3ae80df83b73642
Barba2k2 <lorenzo.dz@hotmail.com>
2025-07-06 16:55:36 -0300

    refactor: update home repository interface

--- lib/app/features/home/domain/repositories/home_repository.dart
+++ lib/app/features/home/domain/repositories/home_repository.dart
@@ -1,7 +1,23 @@
 import '../../../../utils/utils.dart';
+import '../entities/schedule_list_entity.dart';
 
 abstract class HomeRepository {
-  Future<Result<void>> initializeHome();
-  Future<Result<void>> startPolling();
+  Future<Result<List<dynamic>>> fetchSchedules();
+  Future<Result<List<ScheduleListEntity>>> fetchScheduleLists();
+  Future<Result<List<String>>> getAvailableListNames();
+  Future<Result<ScheduleListEntity?>> fetchScheduleListByName(String listName);
+  Future<Result<void>> updateLists();
+  Future<Result<String?>> fetchCurrentChannel();
+  Future<Result<String?>> fetchCurrentChannelForList(String listName);
+  Future<Result<void>> saveScore(
+    int streamerId,
+    DateTime date,
+    int hour,
+    int minute,
+    int points,
+  );
+  Future<Result<void>> startPolling(int streamerId);
   Future<Result<void>> stopPolling();
+  Future<Result<void>> loadUrl(String url);
+  Future<Result<void>> reloadWebView();
 }
