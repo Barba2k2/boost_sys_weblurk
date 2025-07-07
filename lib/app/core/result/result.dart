4967da7de4e45ef3fe047ded07393ef3727a3e45
Barba2k2 <lorenzo.dz@hotmail.com>
2025-07-06 21:09:22 -0300

    feat(result): encapsulate result_dart with AppResult, AppSuccess, AppFailure and adapter

---
+++ lib/app/core/result/result.dart
@@ -0,0 +1,52 @@
+import 'package:result_dart/result_dart.dart' as rd;
+
+/// Interface para padronizar o uso de Result no projeto
+abstract class AppResult<T> {
+  bool get isSuccess;
+  bool get isError;
+  T? get data;
+  Exception? get error;
+}
+
+/// Wrapper para sucesso
+class AppSuccess<T> extends AppResult<T> {
+  final T value;
+  AppSuccess(this.value);
+
+  @override
+  bool get isSuccess => true;
+
+  @override
+  bool get isError => false;
+
+  @override
+  T? get data => value;
+
+  @override
+  Exception? get error => null;
+}
+
+/// Wrapper para erro
+class AppFailure<T> extends AppResult<T> {
+  final Exception exception;
+  AppFailure(this.exception);
+
+  @override
+  bool get isSuccess => false;
+
+  @override
+  bool get isError => true;
+
+  @override
+  T? get data => null;
+
+  @override
+  Exception? get error => exception;
+}
+
+/// Funções utilitárias para conversão
+extension ResultDartAdapter<T> on rd.Result<T, Exception> {
+  AppResult<T> toAppResult() {
+    return isSuccess() ? AppSuccess<T>(getOrNull() as T) : AppFailure<T>(exceptionOrNull()!);
+  }
+} 
