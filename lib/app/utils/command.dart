8d98a82059d122cd33ac83a2f69d5e065901893d
Barba2k2 <lorenzo.dz@hotmail.com>
2025-07-06 21:06:58 -0300

    refactor(result): migrate Command to result_dart/result_command

--- lib/app/utils/command.dart
+++ lib/app/utils/command.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/foundation.dart';
-import 'result.dart';
+import 'package:result_dart/result_dart.dart';
 
 abstract class Command<T> extends ChangeNotifier {
   Command();
   bool _running = false;
-  Result<T>? _result;
+  Result<T, Exception>? _result;
 
   bool get running => _running;
-  Result<T>? get result => _result;
-  bool get error => _result?.isError ?? false;
+  Result<T, Exception>? get result => _result;
+  bool get error => _result?.isError() ?? false;
   bool get completed => _result != null;
 
-  Future<void> _execute(Future<Result<T>> Function() action) async {
+  Future<void> _execute(Future<Result<T, Exception>> Function() action) async {
     if (_running) return;
     _running = true;
     _result = null;
@@ -27,12 +27,12 @@ abstract class Command<T> extends ChangeNotifier {
 
 class Command0<T> extends Command<T> {
   Command0(this._action);
-  final Future<Result<T>> Function() _action;
+  final Future<Result<T, Exception>> Function() _action;
   Future<void> execute() async => _execute(_action);
 }
 
 class Command1<T, A> extends Command<T> {
   Command1(this._action);
-  final Future<Result<T>> Function(A) _action;
+  final Future<Result<T, Exception>> Function(A) _action;
   Future<void> execute(A arg) async => _execute(() => _action(arg));
 } 
