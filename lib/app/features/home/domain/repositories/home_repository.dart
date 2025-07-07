27bb2d9dde83dbb22c5f42b9125b098657333a92
Barba2k2 <lorenzo.dz@hotmail.com>
2025-07-06 22:37:17 -0300

    feat: add startPolling method to HomeRepository interface

--- lib/app/features/home/domain/repositories/home_repository.dart
+++ lib/app/features/home/domain/repositories/home_repository.dart
@@ -1,24 +1,23 @@
-import '../../../../utils/utils.dart';
+import '../../../../core/result/result.dart';
 import '../entities/schedule_list_entity.dart';
-import 'package:result_dart/result_dart.dart';
 
 abstract class HomeRepository {
-  Future<Result<List<dynamic>, Exception>> fetchSchedules();
-  Future<Result<List<ScheduleListEntity>, Exception>> fetchScheduleLists();
-  Future<Result<List<String>, Exception>> getAvailableListNames();
-  Future<Result<ScheduleListEntity?, Exception>> fetchScheduleListByName(String listName);
-  Future<Result<void, Exception>> updateLists();
-  Future<Result<String?, Exception>> fetchCurrentChannel();
-  Future<Result<String?, Exception>> fetchCurrentChannelForList(String listName);
-  Future<Result<void, Exception>> saveScore(
+  Future<AppResult<List<dynamic>>> fetchSchedules();
+  Future<AppResult<List<ScheduleListEntity>>> fetchScheduleLists();
+  Future<AppResult<List<String>>> getAvailableListNames();
+  Future<AppResult<ScheduleListEntity?>> fetchScheduleListByName(String listName);
+  Future<AppResult<void>> updateLists();
+  Future<AppResult<String?>> fetchCurrentChannel();
+  Future<AppResult<String?>> fetchCurrentChannelForList(String listName);
+  Future<AppResult<void>> saveScore(
     int streamerId,
     DateTime date,
     int hour,
     int minute,
     int points,
   );
-  Future<Result<void, Exception>> startPolling(int streamerId);
-  Future<Result<void, Exception>> stopPolling();
-  Future<Result<void, Exception>> loadUrl(String url);
-  Future<Result<void, Exception>> reloadWebView();
+  Future<AppResult<void>> startPolling(int streamerId);
+  Future<AppResult<void>> stopPolling();
+  Future<AppResult<void>> loadUrl(String url);
+  Future<AppResult<void>> reloadWebView();
 }
