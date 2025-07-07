dfe171eef644321f958139bc614b7a301ec8a31c
Barba2k2 <lorenzo.dz@hotmail.com>
2025-07-06 22:37:30 -0300

    feat: add startPolling method to PollingRepository interface

--- lib/app/features/home/domain/repositories/polling_repository.dart
+++ lib/app/features/home/domain/repositories/polling_repository.dart
@@ -1,11 +1,10 @@
-import '../../../../utils/utils.dart';
-import 'package:result_dart/result_dart.dart';
+import '../../../../core/result/result.dart';
 
 abstract class PollingRepository {
-  Future<Result<void, Exception>> startPolling(int streamerId);
-  Future<Result<void, Exception>> stopPolling();
-  Future<Result<void, Exception>> checkAndUpdateChannel();
-  Future<Result<void, Exception>> checkAndUpdateScore(int streamerId);
+  Future<AppResult<void>> startPolling(int streamerId);
+  Future<AppResult<void>> stopPolling();
+  Future<AppResult<void>> checkAndUpdateChannel();
+  Future<AppResult<void>> checkAndUpdateScore(int streamerId);
   Stream<bool> get healthStatus;
   Stream<String> get channelUpdates;
   bool isPollingActive();
