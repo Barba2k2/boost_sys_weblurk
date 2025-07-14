d0353ed8ae0ca2fc1ddd8af24f255ca9f122e8a0
Lorenzo DZ <lorenzo.dz@hotmail.com>
2025-07-13 21:47:52 -0300

    refactor: update main entry point

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,12 @@ import 'package:flutter_modular/flutter_modular.dart';
 import 'app/app_module.dart';
 import 'app/app_widget.dart';
 import 'app/core/application_config.dart';
+import 'app/core/services/update_service.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await ApplicationConfig().consfigureApp();
+  await UpdateService.instance.initialize();
 
   runApp(
     ModularApp(
