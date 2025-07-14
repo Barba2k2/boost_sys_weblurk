8400cede0dd568705080e732e9bff44fc1671e41
Lorenzo DZ <lorenzo.dz@hotmail.com>
2025-07-13 21:47:24 -0300

    refactor: update auth module bindings

--- lib/app/modules/core/auth/auth_module.dart
+++ lib/app/modules/core/auth/auth_module.dart
@@ -1,38 +1,35 @@
 import 'package:flutter_modular/flutter_modular.dart';
 
+import '../../../core/local_storage/flutter_secure_storage/flutter_secure_storage_local_storage_impl.dart';
 import '../../../core/local_storage/local_storage.dart';
 import '../../../core/logger/app_logger.dart';
+import '../../../core/rest_client/rest_client.dart';
 import '../../../repositories/user/user_repository.dart';
 import '../../../repositories/user/user_repository_impl.dart';
 import '../../../service/user/user_service.dart';
 import '../../../service/user/user_service_impl.dart';
 import '../../auth/home/auth_home_page.dart';
 import 'auth_store.dart';
-import 'login/login_controller.dart';
 import 'login/login_module.dart';
 
 class AuthModule extends Module {
   @override
   List<Bind> get binds => [
+        Bind.lazySingleton<LocalSecureStorage>(
+          (i) => FlutterSecureStorageLocalStorageImpl(),
+        ),
         Bind.lazySingleton<UserRepository>(
           (i) => UserRepositoryImpl(
-            logger: i(),
-            restClient: i(),
+            logger: i.get<AppLogger>(),
+            restClient: i.get<RestClient>(),
           ),
         ),
         Bind.lazySingleton<UserService>(
           (i) => UserServiceImpl(
-            logger: i(),
-            userRepository: i(),
-            localStorage: i(),
-            localSecureStorage: i(),
-          ),
-        ),
-        Bind.lazySingleton(
-          (i) => LoginController(
-            userService: i.get<UserService>(),
-            localStorage: i.get<LocalStorage>(),
             logger: i.get<AppLogger>(),
+            userRepository: i.get<UserRepository>(),
+            localStorage: i.get<LocalStorage>(),
+            localSecureStorage: i.get<LocalSecureStorage>(),
           ),
         ),
         Bind.lazySingleton(
