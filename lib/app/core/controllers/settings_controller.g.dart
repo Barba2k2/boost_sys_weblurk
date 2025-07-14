f0624cc91f0b339e39ef66a92245cbb5e8e7c26e
Lorenzo DZ <lorenzo.dz@hotmail.com>
2025-07-13 21:47:13 -0300

    fix: update generated settings controller

--- lib/app/core/controllers/settings_controller.g.dart
+++ lib/app/core/controllers/settings_controller.g.dart
@@ -9,6 +9,22 @@ part of 'settings_controller.dart';
 // ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic, no_leading_underscores_for_local_identifiers
 
 mixin _$SettingsController on SettingsControllerBase, Store {
+  late final _$isAudioMutedAtom =
+      Atom(name: 'SettingsControllerBase.isAudioMuted', context: context);
+
+  @override
+  bool get isAudioMuted {
+    _$isAudioMutedAtom.reportRead();
+    return super.isAudioMuted;
+  }
+
+  @override
+  set isAudioMuted(bool value) {
+    _$isAudioMutedAtom.reportWrite(value, super.isAudioMuted, () {
+      super.isAudioMuted = value;
+    });
+  }
+
   late final _$terminateAppAsyncAction =
       AsyncAction('SettingsControllerBase.terminateApp', context: context);
 
@@ -28,7 +44,7 @@ mixin _$SettingsController on SettingsControllerBase, Store {
   @override
   String toString() {
     return '''
-
+isAudioMuted: ${isAudioMuted}
     ''';
   }
 }
