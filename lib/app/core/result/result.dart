471a232b3b2a0386a618928b859c3c77a6a2d675
Barba2k2 <lorenzo.dz@hotmail.com>
2025-07-20 15:36:14 -0300

    refactor: update result class

--- lib/app/core/result/result.dart
+++ lib/app/core/result/result.dart
@@ -1,4 +1,11 @@
-import 'package:result_dart/result_dart.dart' as rd;
+import 'package:result_dart/result_dart.dart';
+
+/// Encapsulamento para Unit do result_dart
+class AppUnit {
+  const AppUnit._();
+}
+
+const appUnit = AppUnit._();
 
 /// Interface para padronizar o uso de Result no projeto
 abstract class AppResult<T> {
@@ -6,30 +13,68 @@ abstract class AppResult<T> {
   bool get isError;
   T? get data;
   Exception? get error;
+
+  AppResult<R> map<R>(R Function(T value) fn);
+  AppResult<R> flatMap<R>(AppResult<R> Function(T value) fn);
+  ResultDart<Object, Exception> get raw;
 }
 
 /// Wrapper para sucesso
 class AppSuccess<T> extends AppResult<T> {
-  final T value;
-  AppSuccess(this.value);
+  AppSuccess(T value)
+      : _raw = value is AppUnit
+            ? const Success<Unit, Exception>(unit)
+            : Success<Object, Exception>(value as Object);
+  final ResultDart<Object, Exception> _raw;
 
   @override
-  bool get isSuccess => true;
+  bool get isSuccess => _raw.isSuccess();
 
   @override
-  bool get isError => false;
+  bool get isError => _raw.isError();
 
   @override
-  T? get data => value;
+  T? get data =>
+      _raw is Success<Unit, Exception> ? appUnit as T : _raw.getOrNull() as T?;
 
   @override
   Exception? get error => null;
+
+  @override
+  AppResult<R> map<R>(R Function(T value) fn) {
+    if (isSuccess) {
+      try {
+        return AppSuccess<R>(fn(data as T));
+      } catch (e) {
+        return AppFailure(Exception(e.toString()));
+      }
+    } else {
+      return AppFailure(error!);
+    }
+  }
+
+  @override
+  AppResult<R> flatMap<R>(AppResult<R> Function(T value) fn) {
+    if (isSuccess) {
+      try {
+        return fn(data as T);
+      } catch (e) {
+        return AppFailure(Exception(e.toString()));
+      }
+    } else {
+      return AppFailure(error!);
+    }
+  }
+
+  @override
+  ResultDart<Object, Exception> get raw => _raw;
 }
 
 /// Wrapper para erro
 class AppFailure<T> extends AppResult<T> {
-  final Exception exception;
-  AppFailure(this.exception);
+  AppFailure(this._error) : _raw = Failure<Object, Exception>(_error);
+  final Exception _error;
+  final ResultDart<Object, Exception> _raw;
 
   @override
   bool get isSuccess => false;
@@ -41,5 +86,15 @@ class AppFailure<T> extends AppResult<T> {
   T? get data => null;
 
   @override
-  Exception? get error => exception;
-} 
+  Exception? get error => _error;
+
+  @override
+  AppResult<R> map<R>(R Function(T value) fn) => AppFailure<R>(_error);
+
+  @override
+  AppResult<R> flatMap<R>(AppResult<R> Function(T value) fn) =>
+      AppFailure<R>(_error);
+
+  @override
+  ResultDart<Object, Exception> get raw => _raw;
+}
