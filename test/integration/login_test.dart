a1589edd40c7fcb38a4d19cb75a8e24eb3b17eed
Barba2k2 <lorenzo.dz@hotmail.com>
2025-07-20 15:38:24 -0300

    test: update login test

--- test/integration/login_test.dart
+++ test/integration/login_test.dart
@@ -8,7 +8,7 @@ void main() {
         (WidgetTester tester) async {
       // Act
       await tester.pumpWidget(
-        MaterialApp(
+        const MaterialApp(
           home: AuthPage(),
         ),
       );
@@ -21,7 +21,7 @@ void main() {
         (WidgetTester tester) async {
       // Act
       await tester.pumpWidget(
-        MaterialApp(
+        const MaterialApp(
           home: AuthPage(),
         ),
       );
@@ -33,7 +33,7 @@ void main() {
     testWidgets('should handle page rendering', (WidgetTester tester) async {
       // Act
       await tester.pumpWidget(
-        MaterialApp(
+        const MaterialApp(
           home: AuthPage(),
         ),
       );
