caa7900db43a1f9fb609b80c0e2a2cb4df195ab6
Barba2k2 <lorenzo.dz@hotmail.com>
2025-07-20 15:36:46 -0300

    refactor: update auth page

--- lib/app/features/auth/presentation/pages/auth_page.dart
+++ lib/app/features/auth/presentation/pages/auth_page.dart
@@ -1,14 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:validatorless/validatorless.dart';
 
-import '../../../../core/ui/widgets/boost_text_form_field.dart';
 import '../../../../core/di/dependency_injection.dart';
-import '../../domain/entities/auth_store.dart';
-import '../../domain/repositories/auth_repository.dart';
+import '../../../../core/ui/widgets/boost_text_form_field.dart';
+import '../../../../core/ui/widgets/messages/messages.dart';
+import '../../domain/entities/auth_state.dart';
 import '../viewmodels/auth_viewmodel.dart';
-
-import 'package:boost_sys_weblurk/app/core/di/dependency_injection.dart';
 
 class AuthPage extends StatefulWidget {
   const AuthPage({super.key});
@@ -22,14 +21,32 @@ class _AuthPageState extends State<AuthPage> {
   final _nicknameEC = TextEditingController();
   final _passwordEC = TextEditingController();
   late final AuthViewModel _viewModel;
+  late final AuthState _authState;
 
   @override
   void initState() {
     super.initState();
+    _authState = getIt<AuthState>();
     _viewModel = AuthViewModel(
-      repository: getIt<AuthRepository>(),
-      authStore: getIt<AuthStore>(),
+      repository: getIt(),
+      authState: _authState,
+      secureStorage: getIt(),
     );
+
+    _viewModel.login.addListener(() {
+      if (mounted && _viewModel.login.error) {
+        final error = _viewModel.login.result?.error;
+        if (error != null) {
+          Messages.error(error.toString());
+        }
+      }
+    });
+
+    _authState.addListener(() {
+      if (mounted && _authState.isLoggedIn) {
+        context.go('/home');
+      }
+    });
   }
 
   @override
@@ -66,80 +83,94 @@ class _AuthPageState extends State<AuthPage> {
               fit: BoxFit.cover,
             ),
           ),
-          Align(
-            child: Container(
-              decoration: BoxDecoration(
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.5),
-                    blurRadius: 10,
-                    spreadRadius: 5,
-                    offset: const Offset(0, 5),
-                  ),
-                ],
-                color: Colors.purple[800],
-                borderRadius: BorderRadius.circular(20),
-              ),
-              padding: const EdgeInsets.all(32),
-              width: 500,
-              height: 380,
-              child: Form(
-                key: _formKey,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'BoostTeam SysWebLurk',
-                      style: GoogleFonts.poppins(
-                        color: Colors.white,
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
-                        letterSpacing: 1.2,
+          ListenableBuilder(
+            listenable: _viewModel.login,
+            builder: (context, child) {
+              return Align(
+                child: Container(
+                  decoration: BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.5),
+                        blurRadius: 10,
+                        spreadRadius: 5,
+                        offset: const Offset(0, 5),
                       ),
-                    ),
-                    const SizedBox(height: 20),
-                    BoostTextFormField(
-                      controller: _nicknameEC,
-                      label: 'Usuário',
-                      validator: Validatorless.required('Login obrigatório'),
-                    ),
-                    const SizedBox(height: 16),
-                    BoostTextFormField(
-                      controller: _passwordEC,
-                      label: 'Password',
-                      validator: Validatorless.required('Senha obrigatória'),
-                      obscureText: true,
-                    ),
-                    const SizedBox(height: 20),
-                    SizedBox(
-                      width: 200,
-                      height: 50,
-                      child: ElevatedButton(
-                        onPressed: _handleLogin,
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.amber[900],
-                          padding: const EdgeInsets.symmetric(
-                            horizontal: 32,
-                            vertical: 16,
-                          ),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                        ),
-                        child: Text(
-                          'Entrar',
+                    ],
+                    color: Colors.purple[800],
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  padding: const EdgeInsets.all(32),
+                  width: 500,
+                  height: 380,
+                  child: Form(
+                    key: _formKey,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'BoostTeam SysWebLurk',
                           style: GoogleFonts.poppins(
                             color: Colors.white,
-                            fontSize: 16,
+                            fontSize: 24,
                             fontWeight: FontWeight.bold,
+                            letterSpacing: 1.2,
                           ),
                         ),
-                      ),
+                        const SizedBox(height: 20),
+                        BoostTextFormField(
+                          controller: _nicknameEC,
+                          label: 'Usuário',
+                          validator:
+                              Validatorless.required('Login obrigatório'),
+                        ),
+                        const SizedBox(height: 16),
+                        BoostTextFormField(
+                          controller: _passwordEC,
+                          label: 'Senha',
+                          validator:
+                              Validatorless.required('Senha obrigatória'),
+                          obscureText: true,
+                        ),
+                        const SizedBox(height: 20),
+                        SizedBox(
+                          width: 200,
+                          height: 50,
+                          child: ElevatedButton(
+                            onPressed:
+                                _viewModel.login.running ? null : _handleLogin,
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.amber[900],
+                              padding: const EdgeInsets.symmetric(
+                                horizontal: 32,
+                                vertical: 16,
+                              ),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                            ),
+                            child: _viewModel.login.running
+                                ? const CircularProgressIndicator(
+                                    valueColor: AlwaysStoppedAnimation<Color>(
+                                      Colors.white,
+                                    ),
+                                  )
+                                : Text(
+                                    'Entrar',
+                                    style: GoogleFonts.poppins(
+                                      color: Colors.white,
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                          ),
+                        ),
+                      ],
                     ),
-                  ],
+                  ),
                 ),
-              ),
-            ),
+              );
+            },
           ),
         ],
       ),
