044206033d0f7bb984435eef2e5bedcac5cc8e44
Barba2k2 <lorenzo.dz@hotmail.com>
2025-07-20 15:37:00 -0300

    refactor: update webview repository implementation

--- lib/app/features/home/data/repositories/webview_repository_impl.dart
+++ lib/app/features/home/data/repositories/webview_repository_impl.dart
@@ -1,28 +1,24 @@
 import 'package:desktop_webview_window/desktop_webview_window.dart';
-import 'package:result_dart/result_dart.dart';
 
 import '../../../../core/logger/app_logger.dart';
-import '../../../../utils/utils.dart';
 import '../../domain/repositories/webview_repository.dart';
-import '../datasources/webview_datasource.dart';
+import '../../domain/datasources/webview_data_source.dart';
 import '../../../../core/result/result.dart';
 
 class WebViewRepositoryImpl implements WebViewRepository {
-  final WebViewDataSource _dataSource;
-  final AppLogger _logger;
-
   WebViewRepositoryImpl({
     required WebViewDataSource dataSource,
     required AppLogger logger,
   })  : _dataSource = dataSource,
         _logger = logger;
+  final WebViewDataSource _dataSource;
+  final AppLogger _logger;
 
   @override
-  Future<AppResult<void>> initializeWebView(Webview controller) async {
+  Future<AppResult<AppUnit>> initializeWebView(Webview controller) async {
     try {
       _logger.info('Inicializando WebView via repository');
-      await _dataSource.initializeWebView(controller);
-      return AppSuccess(null);
+      return await _dataSource.initializeWebView(controller);
     } catch (e, s) {
       _logger.error('Erro inesperado ao inicializar WebView', e, s);
       return AppFailure(Exception('Erro ao inicializar WebView: $e'));
@@ -30,11 +26,10 @@ class WebViewRepositoryImpl implements WebViewRepository {
   }
 
   @override
-  Future<AppResult<void>> loadUrl(String url) async {
+  Future<AppResult<AppUnit>> loadUrl(String url) async {
     try {
       _logger.info('Carregando URL via repository: $url');
-      await _dataSource.loadUrl(url);
-      return AppSuccess(null);
+      return await _dataSource.loadUrl(url);
     } catch (e, s) {
       _logger.error('Erro inesperado ao carregar URL', e, s);
       return AppFailure(Exception('Erro ao carregar URL: $e'));
@@ -42,11 +37,10 @@ class WebViewRepositoryImpl implements WebViewRepository {
   }
 
   @override
-  Future<AppResult<void>> reload() async {
+  Future<AppResult<AppUnit>> reload() async {
     try {
       _logger.info('Recarregando WebView via repository');
-      await _dataSource.reload();
-      return AppSuccess(null);
+      return await _dataSource.reload();
     } catch (e, s) {
       _logger.error('Erro inesperado ao recarregar WebView', e, s);
       return AppFailure(Exception('Erro ao recarregar WebView: $e'));
@@ -57,8 +51,7 @@ class WebViewRepositoryImpl implements WebViewRepository {
   Future<AppResult<bool>> isResponding() async {
     try {
       _logger.info('Verificando resposta do WebView via repository');
-      final result = await _dataSource.isResponding();
-      return AppSuccess(result);
+      return await _dataSource.isResponding();
     } catch (e, s) {
       _logger.error('Erro inesperado ao verificar resposta do WebView', e, s);
       return AppFailure(Exception('Erro ao verificar resposta do WebView: $e'));
@@ -79,4 +72,4 @@ class WebViewRepositoryImpl implements WebViewRepository {
 
   @override
   void dispose() => _dataSource.dispose();
-} 
+}
