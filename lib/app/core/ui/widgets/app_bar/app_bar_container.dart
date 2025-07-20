8cf45f83cd97eba3ffa9fa794c0d93d44b70b684
Barba2k2 <lorenzo.dz@hotmail.com>
2025-07-20 15:36:17 -0300

    refactor: update app bar container

--- lib/app/core/ui/widgets/app_bar/app_bar_container.dart
+++ lib/app/core/ui/widgets/app_bar/app_bar_container.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 
 class AppBarContainer extends StatelessWidget {
-  final Widget child;
-
   const AppBarContainer({
     super.key,
     required this.child,
   });
+
+  final Widget child;
 
   @override
   Widget build(BuildContext context) {
@@ -15,7 +15,7 @@ class AppBarContainer extends StatelessWidget {
         color: const Color(0xFF2C1F4A),
         boxShadow: [
           BoxShadow(
-            color: Colors.black.withOpacity(0.1),
+            color: Colors.black.withValues(alpha: 0.1),
             blurRadius: 4,
             offset: const Offset(0, 2),
           ),
@@ -24,4 +24,4 @@ class AppBarContainer extends StatelessWidget {
       child: child,
     );
   }
-} 
+}
