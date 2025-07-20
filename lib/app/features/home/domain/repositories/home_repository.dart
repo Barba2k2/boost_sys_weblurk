67855bf9c8d47258ef8fdf669b6fb7c47750c8f1
Barba2k2 <lorenzo.dz@hotmail.com>
2025-07-20 15:37:12 -0300

    refactor: update home repository interface

--- lib/app/features/home/domain/repositories/home_repository.dart
+++ lib/app/features/home/domain/repositories/home_repository.dart
@@ -5,19 +5,21 @@ abstract class HomeRepository {
   Future<AppResult<List<dynamic>>> fetchSchedules();
   Future<AppResult<List<ScheduleListEntity>>> fetchScheduleLists();
   Future<AppResult<List<String>>> getAvailableListNames();
-  Future<AppResult<ScheduleListEntity?>> fetchScheduleListByName(String listName);
-  Future<AppResult<void>> updateLists();
+  Future<AppResult<ScheduleListEntity?>> fetchScheduleListByName(
+    String listName,
+  );
+  Future<AppResult<AppUnit>> updateLists();
   Future<AppResult<String?>> fetchCurrentChannel();
   Future<AppResult<String?>> fetchCurrentChannelForList(String listName);
-  Future<AppResult<void>> saveScore(
+  Future<AppResult<AppUnit>> saveScore(
     int streamerId,
     DateTime date,
     int hour,
     int minute,
     int points,
   );
-  Future<AppResult<void>> startPolling(int streamerId);
-  Future<AppResult<void>> stopPolling();
-  Future<AppResult<void>> loadUrl(String url);
-  Future<AppResult<void>> reloadWebView();
+  Future<AppResult<AppUnit>> startPolling(int streamerId);
+  Future<AppResult<AppUnit>> stopPolling();
+  Future<AppResult<AppUnit>> loadUrl(String url);
+  Future<AppResult<AppUnit>> reloadWebView();
 }
