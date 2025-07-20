40cc63ab9a75119036b4684a87d8c237e3550422
Barba2k2 <lorenzo.dz@hotmail.com>
2025-07-20 15:38:19 -0300

    refactor: update main entry point

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,11 @@ import 'package:flutter/material.dart';
 
 import 'app/app_widget.dart';
 import 'app/core/application_config.dart';
-import 'app/core/di/di.dart';
+import 'app/core/di/dependency_injection.dart';
 
 Future<void> main() async {
-  await ApplicationConfig().consfigureApp();
+  WidgetsFlutterBinding.ensureInitialized();
+  await ApplicationConfig().configureApp();
 
   // Initialize dependency injection
   di.initialize();
