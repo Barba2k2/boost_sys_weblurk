2d01b226d87ceae44a417d8db9af60af81557b5f
Barba2k2 <lorenzo.dz@hotmail.com>
2025-07-20 15:36:35 -0300

    refactor: update auth module exports

--- lib/app/features/auth/auth.dart
+++ lib/app/features/auth/auth.dart
@@ -1,6 +1,6 @@
 // Auth feature barrel
 export 'data/repositories/auth_repository_impl.dart';
-export 'domain/entities/auth_store.dart';
+export 'domain/entities/auth_state.dart';
 export 'domain/entities/user_entity.dart';
 export 'domain/repositories/auth_repository.dart';
 export 'presentation/viewmodels/auth_viewmodel.dart';
