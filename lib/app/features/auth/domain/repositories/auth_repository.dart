f97269c562e96f5a369b5b3c44a0327ed1fa3bca
Barba2k2 <lorenzo.dz@hotmail.com>
2025-07-20 15:36:42 -0300

    refactor: update auth repository interface

--- lib/app/features/auth/domain/repositories/auth_repository.dart
+++ lib/app/features/auth/domain/repositories/auth_repository.dart
@@ -1,8 +1,10 @@
-import '../entities/user_entity.dart';
 import '../../../../core/result/result.dart';
 
 abstract class AuthRepository {
-  Future<AppResult<Map<String, dynamic>>> login(String nickname, String password);
-  Future<AppResult<void>> logout();
+  Future<AppResult<Map<String, dynamic>>> login(
+    String nickname,
+    String password,
+  );
+  Future<AppResult<AppUnit>> logout();
   Future<AppResult<bool>> checkLoginStatus();
 }
