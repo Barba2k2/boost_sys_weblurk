49e56c89126860485575c40ffc68311345aad1f7
Lorenzo DZ <lorenzo.dz@hotmail.com>
2025-07-22 08:04:21 -0300

    fix(user): update user service interface

--- lib/app/service/user/user_service.dart
+++ lib/app/service/user/user_service.dart
@@ -2,4 +2,5 @@ abstract class UserService {
   Future<void> login(String nickname, String password);
   Future<void> logout();
   Future<String?> getToken();
+  Future<void> updateLoginStatus(String status);
 }
