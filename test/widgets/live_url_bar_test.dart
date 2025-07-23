db0fe211ef12bed2578f226d357153842e44424c
Lorenzo DZ <lorenzo.dz@hotmail.com>
2025-07-23 00:51:11 -0300

    test: update live_url_bar_test.dart

--- test/widgets/live_url_bar_test.dart
+++ test/widgets/live_url_bar_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:boost_sys_weblurk/app/core/ui/widgets/live_url_bar.dart';
+import 'package:boost_sys_weblurk/core/ui/widgets/live_url_bar.dart';
 
 void main() {
   group(
