5a50d58546067919207c8c0f3b4d256fc99e9f49
Lorenzo DZ <lorenzo.dz@hotmail.com>
2025-07-23 00:45:09 -0300

    feat(core): add application_config.dart

---
+++ lib/core/application_config.dart
@@ -0,0 +1,43 @@
+import 'dart:io';
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+import 'package:window_manager/window_manager.dart';
+
+import 'helpers/environments.dart';
+import 'services/update_service.dart';
+
+class ApplicationConfig {
+  Future<void> consfigureApp() async {
+    WidgetsFlutterBinding.ensureInitialized();
+
+    await _loadEnvs();
+
+    if (Platform.isWindows) {
+      await _configureWindowManager();
+    } else if (Platform.isAndroid) {
+      await _initializeUpdateService();
+    }
+  }
+
+  Future<void> _loadEnvs() => Environments.loadEnvs();
+
+  Future<void> _configureWindowManager() async {
+    try {
+      await windowManager.ensureInitialized();
+
+      windowManager.setSize(
+        const Size(1014, 624),
+      );
+
+      windowManager.setResizable(false);
+    } catch (e, s) {
+      log('Error configuring window manager: $e');
+      log('StackTrace: $s');
+    }
+  }
+
+  Future<void> _initializeUpdateService() async {
+    await UpdateService.instance.initialize();
+  }
+}
