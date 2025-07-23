7f5125480aa4be5c681b61289e2cd041b9815525
Barba2k2 <lorenzo.dz@hotmail.com>
2025-07-23 11:09:31 -0300

    refactor(core): centralize all colors in AppColors

---
+++ lib/core/ui/app_colors.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  static const Color primary = Color(0xFF6750A4);
+  static const Color secondary = Color(0xFF625B71);
+  static const Color tertiary = Color(0xFF7D5260);
+  static const Color background = Color(0xFF231942);
+  static const Color surface = Color(0xFF2C1F4A);
+  static const Color accent = Color(0xFFA259FF);
+  static const Color appBar = Color(0xFF2C1F4A);
+  static const Color tabBarLabel = Color(0xFF2C1F4A);
+  static const Color tabBarUnselected = Color(0xFF757575);
+  static const Color tabBarIndicator = Color(0xFF2C1F4A);
+  static const Color cardHeader = Color(0xFF2C1F4A);
+  static const Color cardHeaderText = Colors.white;
+  static const Color cardHeaderSubText = Colors.white70;
+  static const Color cardBackground = Colors.white;
+  static const Color cardBackgroundOpacity = Color(0xCCFFFFFF);
+  static const Color menuButton = Color(0x1FFFFFFF);
+  static const Color menuButtonActive = Color(0xFFA162FF);
+  static const Color menuItemIcon = Colors.white;
+  static const Color menuItemIconInactive = Colors.black;
+  static const Color loader = Colors.purple;
+  static const Color loaderBackground = Colors.black54;
+  static const Color error = Colors.red;
+  static const Color warning = Color(0xFFFF9800);
+  static const Color success = Color(0xFF4CAF50);
+  static const Color info = Color(0xFF2196F3);
+  static const Color scheduleRowActive = Color(0x1A4CAF50);
+  static const Color scheduleRowPending = Color(0x1AFFA726);
+  static const Color scheduleRowCancelled = Color(0x1AF44336);
+  static const Color translucentField = Color(0x42000000);
+  static const Color transparent = Colors.transparent;
+}
