d9f4260241106da516612d69a69a5908cbc0556e
Lorenzo DZ <lorenzo.dz@hotmail.com>
2025-07-24 16:37:50 -0300

    test: add url_launcher_service_test

---
+++ test/services/url_launcher_service_test.dart
@@ -0,0 +1,68 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:process_run/process_run.dart';
+import 'package:boost_sys_weblurk/core/logger/app_logger.dart';
+import 'package:boost_sys_weblurk/core/services/url_launcher_service.dart';
+
+// Mock the logger
+class MockAppLogger extends Mock implements AppLogger {}
+
+class MockShell extends Mock implements Shell {}
+
+void registerFallbacks() {
+  registerFallbackValue(<String, String>{});
+}
+
+void main() {
+  group(
+    'UrlLauncherService',
+    () {
+      late MockAppLogger mockLogger;
+      late UrlLauncherService urlLauncherService;
+      late MockShell mockShell;
+
+      setUp(() {
+        mockLogger = MockAppLogger();
+        mockShell = MockShell();
+        registerFallbacks();
+        urlLauncherService = UrlLauncherService(
+          logger: mockLogger,
+          shell: mockShell,
+        );
+      });
+
+      test(
+        'launchURL logs error if URL is invalid',
+        () async {
+          // Arrange
+          const invalidUrl = 'invalid-url';
+
+          // Act
+          await urlLauncherService.launchURL(invalidUrl);
+
+          // Assert
+          verify(() => mockLogger.error(
+                'URL inválida ou maliciosa detectada: $invalidUrl',
+              )).called(1);
+        },
+      );
+
+      test(
+        'launchURL opens URL if valid',
+        () async {
+          // Arrange
+          const validUrl = 'https://twitch.tv/BoostTeam_';
+          when(() => mockShell.run(any())).thenAnswer((_) async => []);
+
+          // Act
+          await urlLauncherService.launchURL(validUrl);
+
+          // Assert
+          final captured = verify(() => mockShell.run(captureAny())).captured;
+          expect(captured.length, 1);
+          expect(captured.first, contains(validUrl));
+        },
+      );
+    },
+  );
+}
