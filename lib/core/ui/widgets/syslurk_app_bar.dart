b37ec0d4fe348fdd2198f7e5d929ab150015a123
Lorenzo DZ <lorenzo.dz@hotmail.com>
2025-07-24 23:28:25 -0300

    fix(ui): update syslurk app bar

--- lib/core/ui/widgets/syslurk_app_bar.dart
+++ lib/core/ui/widgets/syslurk_app_bar.dart
@@ -4,8 +4,8 @@ import '../../../features/home/presentation/viewmodels/home_viewmodel.dart';
 import '../../services/settings_service.dart';
 import '../../services/url_launcher_service.dart';
 import '../../services/volume_service.dart';
-import '../widgets/messages.dart';
 import '../app_colors.dart';
+import 'combined_menu_button.dart';
 
 class SyslurkAppBar extends StatelessWidget implements PreferredSizeWidget {
   final HomeViewModel viewModel;
@@ -42,110 +42,25 @@ class SyslurkAppBar extends StatelessWidget implements PreferredSizeWidget {
         automaticallyImplyLeading: false,
         title: Row(
           children: [
+            CombinedMenuButton(
+              viewModel: viewModel,
+              settingsService: settingsService,
+              urlLauncherService: urlLauncherService,
+              volumeService: volumeService,
+            ),
+            const SizedBox(width: 16),
             Image.asset(
               'assets/images/logo-cla-boost.png',
               height: 32,
             ),
             const SizedBox(width: 16),
-            _buildMenuButton(
-              'Opções',
-              Icons.menu,
-              [
-                _buildMenuItem(
-                  'Atualizar Listas',
-                  Icons.refresh,
-                  () {
-                    viewModel.reloadWebView();
-                  },
-                ),
-                _buildMenuItem(
-                  'Encerrar',
-                  Icons.power_settings_new,
-                  () {
-                    settingsService.terminateApp();
-                  },
-                ),
-                _buildMenuItem(
-                  'Audio',
-                  Icons.volume_up,
-                  () {
-                    volumeService.toggleMute();
-                  },
-                ),
-                _buildMenuItem(
-                  'Fuso Horário',
-                  Icons.schedule,
-                  () {
-                    Messages.info('Funcionalidade ainda não funcional');
-                  },
-                ),
-              ],
-            ),
-            const SizedBox(width: 8),
-            _buildMenuButton(
-              'Links',
-              Icons.link,
-              [
-                _buildMenuItem(
-                  'Pontuação',
-                  Icons.leaderboard,
-                  () async {
-                    await urlLauncherService.launchURL(
-                      'https://docs.google.com/spreadsheets/d/1kh4zc2INhLEOGbLqqte8NnP4NsNRvFTgSWvKNKKM9qk/edit?usp=sharing',
-                    );
-                  },
-                ),
-                _buildMenuItem(
-                  'Discord',
-                  Icons.discord,
-                  () async {
-                    await urlLauncherService.launchURL(
-                      'https://discord.gg/udteYpaGuB',
-                    );
-                  },
-                ),
-                _buildMenuItem(
-                  'Redes Sociais',
-                  Icons.live_tv_rounded,
-                  () async {
-                    await urlLauncherService.launchURL(
-                      'https://www.twitch.com/BoostTeam_',
-                    );
-                  },
-                ),
-                _buildMenuItem(
-                  'Formulário de horários',
-                  Icons.edit_document,
-                  () async {
-                    await urlLauncherService.launchURL(
-                      'https://forms.gle/RN4NGWm8Qvi1daqp7',
-                    );
-                  },
-                ),
-                _buildMenuItem(
-                  'BoostTeam SysWeblurk',
-                  Icons.edit_document,
-                  () async {
-                    await urlLauncherService.launchURL(
-                      'https://drive.google.com/drive/folders/1XsmNh_gpKYLEkMSaBPFLuT3Y5vDmeWC3?usp=sharing',
-                    );
-                  },
-                ),
-              ],
-            ),
-            const SizedBox(width: 8),
-            _buildMenuButton(
-              'Sobre',
-              Icons.info_outline_rounded,
-              [
-                _buildMenuItem(
-                  'Sobre o Weblurk',
-                  Icons.leaderboard,
-                  () async {
-                    Messages.info('Em breve!');
-                  },
-                ),
-              ],
+            Text(
+              'Canal: BoostTeam_',
+              style: GoogleFonts.inter(
+                color: AppColors.menuItemIcon,
+                fontWeight: FontWeight.w500,
+                fontSize: 14,
+              ),
             ),
           ],
         ),
@@ -180,64 +95,6 @@ class SyslurkAppBar extends StatelessWidget implements PreferredSizeWidget {
                 ],
               ),
             ),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildMenuButton(
-    String label,
-    IconData icon,
-    List<PopupMenuItem<String>> items,
-  ) {
-    return PopupMenuButton<String>(
-      offset: const Offset(0, 40),
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
-      itemBuilder: (context) => items,
-      child: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 6),
-        decoration: BoxDecoration(
-          color: AppColors.menuButton,
-          borderRadius: BorderRadius.circular(20),
-        ),
-        child: Row(
-          children: [
-            Text(
-              label,
-              style: GoogleFonts.inter(
-                color: AppColors.menuItemIcon,
-                fontWeight: FontWeight.w500,
-              ),
-            ),
-            const SizedBox(width: 10),
-            Icon(
-              icon,
-              size: 20,
-              color: AppColors.menuItemIcon,
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  PopupMenuItem<String> _buildMenuItem(
-    String label,
-    IconData icon,
-    VoidCallback onTap,
-  ) {
-    return PopupMenuItem<String>(
-      value: label,
-      onTap: onTap,
-      child: Row(
-        children: [
-          Icon(
-            icon,
-            size: 20,
-            color: AppColors.menuItemIconInactive,
-          ),
-          const SizedBox(width: 10),
-          Text(label),
         ],
       ),
     );
