36020d29d3f8f902dd71ef7b542a48c1778b1a89
Lorenzo DZ <lorenzo.dz@hotmail.com>
2025-07-27 11:43:06 -0300

    refactor: clean up home service formatting

--- lib/service/home/home_service.dart
+++ lib/service/home/home_service.dart
@@ -5,7 +5,7 @@ abstract class HomeService {
   Future<List<ScheduleListModel>> fetchScheduleLists();
   Future<List<String>> getAvailableListNames();
   Future<ScheduleListModel?> fetchScheduleListByName(String listName);
-  // Future<void> forceUpdateLive();
+
   Future<void> updateLists();
   Future<String?> fetchCurrentChannel();
   Future<String?> fetchCurrentChannelForList(String listName);
