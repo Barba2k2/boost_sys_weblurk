f82a17521af5136df400c00017ccceaa71ed3c89
Lorenzo DZ <lorenzo.dz@hotmail.com>
2025-07-27 11:21:39 -0300

    refactor: clean up command utility formatting

--- lib/core/utils/command.dart
+++ lib/core/utils/command.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/foundation.dart';
 import 'result.dart';
 
-// Tipos de função para Commands
 typedef CommandAction0<T> = Future<Result<T>> Function();
 typedef CommandAction1<T, A> = Future<Result<T>> Function(A params);
 typedef CommandAction2<T, A, B> = Future<Result<T>> Function(
     A params1, B params2);
 
-// Command base abstrato
 abstract class Command<T> extends ChangeNotifier {
   bool _running = false;
   bool get running => _running;
@@ -33,7 +31,6 @@ abstract class Command<T> extends ChangeNotifier {
   }
 }
 
-// Command sem parâmetros
 class Command0<T> extends Command<T> {
   Command0(this.action);
   final CommandAction0<T> action;
@@ -43,7 +40,6 @@ class Command0<T> extends Command<T> {
   }
 }
 
-// Command com 1 parâmetro
 class Command1<T, A> extends Command<T> {
   Command1(this.action);
   final CommandAction1<T, A> action;
@@ -53,7 +49,6 @@ class Command1<T, A> extends Command<T> {
   }
 }
 
-// Command com 2 parâmetros
 class Command2<T, A, B> extends Command<T> {
   Command2(this.action);
   final CommandAction2<T, A, B> action;
