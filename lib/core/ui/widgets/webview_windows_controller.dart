163cd0d6bf9c44bfd72f5f329824503f24edb6b6
Lorenzo DZ <lorenzo.dz@hotmail.com>
2025-09-07 10:33:08 -0300

    feat: add Windows webview controller implementation

---
+++ lib/core/ui/widgets/webview_windows_controller.dart
@@ -0,0 +1,55 @@
+import 'dart:async';
+import 'package:webview_windows/webview_windows.dart';
+
+import 'webview_controller_interface.dart';
+
+/// Windows implementation of WebViewControllerInterface using webview_windows
+class WebViewWindowsController implements WebViewControllerInterface {
+  final WebviewController _controller = WebviewController();
+  String? _currentUrl;
+
+  @override
+  Future<void> initialize() async {
+    await _controller.initialize();
+  }
+
+  @override
+  Future<void> loadUrl(String url) async {
+    await _controller.loadUrl(url);
+    _currentUrl = url;
+  }
+
+  @override
+  Future<void> executeScript(String script) async {
+    await _controller.executeScript(script);
+  }
+
+  @override
+  Future<void> setBackgroundColor(dynamic color) async {
+    await _controller.setBackgroundColor(color);
+  }
+
+  @override
+  Future<void> setPopupWindowPolicy(dynamic policy) async {
+    await _controller.setPopupWindowPolicy(policy);
+  }
+
+  @override
+  Stream<dynamic> get loadingState => _controller.loadingState;
+
+  @override
+  void dispose() {
+    _controller.dispose();
+  }
+
+  @override
+  String? get currentUrl => _currentUrl;
+
+  @override
+  set currentUrl(String? url) {
+    _currentUrl = url;
+  }
+
+  /// Get the underlying WebviewController for platform-specific operations
+  WebviewController get nativeController => _controller;
+}
